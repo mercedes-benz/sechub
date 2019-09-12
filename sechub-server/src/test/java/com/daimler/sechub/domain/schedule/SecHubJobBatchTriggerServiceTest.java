728ce4cfc5d195828eb648739923ce7f86936382
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2019-09-12 08:06:13 +0200

    Fix broken tests and added additional test #12

--- sechub-server/src/test/java/com/daimler/sechub/domain/schedule/SecHubJobBatchTriggerServiceTest.java
+++ sechub-server/src/test/java/com/daimler/sechub/domain/schedule/SecHubJobBatchTriggerServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.daimler.sechub.domain.schedule.config.SchedulerConfigService;
 import com.daimler.sechub.domain.schedule.job.ScheduleSecHubJob;
 import com.daimler.sechub.sharedkernel.cluster.ClusterEnvironmentService;
 
@@ -17,7 +18,9 @@ public class SecHubJobBatchTriggerServiceTest {
 	private ScheduleJobLauncherService launcherService;
 	private ScheduleJobMarkerService markerService;
 	private ClusterEnvironmentService environmentService;
-	
+
+	private SchedulerConfigService configService;
+
 	@Before
 	public void before() throws Exception {
 		serviceToTest = new SchedulerJobBatchTriggerService();
@@ -25,16 +28,21 @@ public class SecHubJobBatchTriggerServiceTest {
 		launcherService = mock(ScheduleJobLauncherService.class);
 		markerService = mock(ScheduleJobMarkerService.class);
 		environmentService = mock(ClusterEnvironmentService.class);
+		configService=mock(SchedulerConfigService.class);
 
 		serviceToTest.launcherService=launcherService;
 		serviceToTest.markerService=markerService;
 		serviceToTest.environmentService=environmentService;
-		
+		serviceToTest.configService=configService;
+
+
 	}
+
 
 	@Test
 	public void triggerExecutionOfNextJob__calls_marker_service_markNextJobExecutedByThisPOD() throws Exception {
 		/* prepare */
+		when(configService.isJobProcessingEnabled()).thenReturn(true);
 
 		/* execute */
 		serviceToTest.triggerExecutionOfNextJob();
@@ -44,11 +52,27 @@ public class SecHubJobBatchTriggerServiceTest {
 	}
 
 	@Test
+	public void when_processsing_is_disabled_but_marker_service_returns_job_launcher_service_is_NOT_called()
+			throws Exception {
+		/* prepare */
+		ScheduleSecHubJob job = mock(ScheduleSecHubJob.class);
+		when(markerService.markNextJobExecutedByThisPOD()).thenReturn(job);
+		when(configService.isJobProcessingEnabled()).thenReturn(false);
+
+		/* execute */
+		serviceToTest.triggerExecutionOfNextJob();
+
+		/* test */
+		verify(launcherService,never()).executeJob(job);
+	}
+
+	@Test
 	public void when_marker_service_returns_job_launcher_service_is_called()
 			throws Exception {
 		/* prepare */
 		ScheduleSecHubJob job = mock(ScheduleSecHubJob.class);
 		when(markerService.markNextJobExecutedByThisPOD()).thenReturn(job);
+		when(configService.isJobProcessingEnabled()).thenReturn(true);
 
 		/* execute */
 		serviceToTest.triggerExecutionOfNextJob();
@@ -56,11 +80,12 @@ public class SecHubJobBatchTriggerServiceTest {
 		/* test */
 		verify(launcherService).executeJob(job);
 	}
-	
+
 	@Test
 	public void when_marker_service_returns_NO_job_launcher_service_is_NOT_called()
 			throws Exception {
 		/* prepare */
+		when(configService.isJobProcessingEnabled()).thenReturn(true);
 		when(markerService.markNextJobExecutedByThisPOD()).thenReturn(null);
 
 		/* execute */
