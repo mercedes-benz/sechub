ff93622711860a490e892323338d0e388715effd
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2019-10-01 17:15:05 +0200

    Security configuration for server fixed, #55
    - wrong configured web security did fail integration tests
    - fixed by removing former wrong added NOT part in profile
      annotation
    
    On behalf of Daimler TSS GmbH.

--- sechub-server/src/main/java/com/daimler/sechub/server/security/SecHubServerSecurityConfiguration.java
+++ sechub-server/src/main/java/com/daimler/sechub/server/security/SecHubServerSecurityConfiguration.java
@@ -2,18 +2,15 @@
 package com.daimler.sechub.server.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-import com.daimler.sechub.sharedkernel.Profiles;
 import com.daimler.sechub.sharedkernel.configuration.AbstractAllowSecHubAPISecurityConfiguration;
 
 @Configuration
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
 @EnableWebSecurity
-@Profile({"!"+Profiles.INTEGRATIONTEST}) // not for integration test
 @Order(1)
 public class SecHubServerSecurityConfiguration extends AbstractAllowSecHubAPISecurityConfiguration {
 
