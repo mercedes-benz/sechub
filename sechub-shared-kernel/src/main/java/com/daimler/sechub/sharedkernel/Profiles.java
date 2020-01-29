ac5e51728d7b036bee651fb9fc58faca023fb513
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2020-01-29 12:33:33 +0100

    Separation and usage of initial admin profiles, closes #155

--- sechub-shared-kernel/src/main/java/com/daimler/sechub/sharedkernel/Profiles.java
+++ sechub-shared-kernel/src/main/java/com/daimler/sechub/sharedkernel/Profiles.java
@@ -39,7 +39,28 @@ public class Profiles {
 	 */
 	public static final String INTEGRATIONTEST="integrationtest";
 
-
+	/**
+	 * When an initial administrator must be created the predefined values will be used
+	 * (So API token is well known) see InitialAdmininInitializer.java
+	 */
+	public static final String INITIAL_ADMIN_PREDEFINED="initial_admin_predefined";
+	
+	/**
+	 * When an initial administrator must be created a static value will be used
+	 * (So API token is well known) but this is NOT ENCRYPTED. Use this ONLY for (integration) test purposes!
+	 * See InitialAdmininInitializer.java
+	 */
+	public static final String INITIAL_ADMIN_STATIC="initial_admin_static";
+	
+	/**
+	 * The profile ensures an administrator is available inside system on startup.
+	 * If not an initial administrator account will be created. Information about 
+	 * login and further steps can be found inside server logs.
+	 * See InitialAdmininInitializer.java
+	 * 
+	 */
+	public static final String INITIAL_ADMIN_CREATED="initial_admin_created";
+	
 	/**
 	 * Special profile : marked parts are critical and will provide administrative
 	 *  access/ priviledges. You should start different servers: 1..n without
