63bb7f8701efbc64db02c35a6c4f730005f2f534
Sven Dolderer <sven.dolderer@daimler.com>
2020-02-05 13:27:55 +0100

    added build platforms #167
    New platforms:
    - "linux/amd64" Linux default architecture
    - "linux/arm" Raspberry PI
    - "linux/arm64" Raspberry PI/Amazon
    - "darwin/amd64" Mac

--- sechub-cli/build_go.sh
+++ sechub-cli/build_go.sh
@@ -16,7 +16,7 @@ if [[ -z "$package" ]]; then
 fi
 
 if [[ -z "$platforms" ]]; then
-    platforms=("linux/386" "windows/amd64" "windows/386" )
+    platforms=("linux/386" "linux/amd64" "linux/arm" "linux/arm64" "darwin/amd64" "windows/amd64" "windows/386")
 fi
 
 
