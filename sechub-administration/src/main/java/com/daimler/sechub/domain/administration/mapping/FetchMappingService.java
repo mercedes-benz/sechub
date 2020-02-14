91cdcd60d2e03c2f4e9ba92300f19ce7ffc3a2c0
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2020-02-14 14:23:45 +0100

    Mapping identifier improved #129
    - mapping identifier does now provide
      - self sanity check (duplicates, valid id)
      - mapping type
    - scan domain does only accept global or adapter configuration
      mapping types now

--- sechub-administration/src/main/java/com/daimler/sechub/domain/administration/mapping/FetchMappingService.java
+++ sechub-administration/src/main/java/com/daimler/sechub/domain/administration/mapping/FetchMappingService.java
@@ -3,7 +3,6 @@ package com.daimler.sechub.domain.administration.mapping;
 
 import static com.daimler.sechub.sharedkernel.validation.AssertValidResult.*;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
