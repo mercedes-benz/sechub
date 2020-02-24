f926077f41fff6e101dc7b8779272c6cf970efc9
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2020-02-24 15:05:24 +0100

    Changed behavior of TestAPI.waitForScanConfigRefresh, fixes #194
    - TestAPI.waitForScanConfigRefresh new implemented, does now no longer
      wait dedicated time but instead changes scan config part and wait
      until its reloaded
    - added some additional integration test methods in integrationtest
      rest controller + made one method in service public available which
      was protected before (but its now deprecated) to have test api access

--- sechub-scan/src/main/java/com/daimler/sechub/domain/scan/IntegrationTestScanRestController.java
+++ sechub-scan/src/main/java/com/daimler/sechub/domain/scan/IntegrationTestScanRestController.java
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 package com.daimler.sechub.domain.scan;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
@@ -11,6 +13,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daimler.sechub.domain.scan.access.ScanAccessCountService;
+import com.daimler.sechub.domain.scan.config.NamePatternIdprovider;
+import com.daimler.sechub.domain.scan.config.ScanConfigService;
+import com.daimler.sechub.domain.scan.config.ScanMapping;
+import com.daimler.sechub.domain.scan.config.ScanMappingRepository;
 import com.daimler.sechub.domain.scan.config.UpdateScanMappingService;
 import com.daimler.sechub.domain.scan.product.ProductResultCountService;
 import com.daimler.sechub.domain.scan.report.ScanReportCountService;
@@ -19,7 +25,8 @@ import com.daimler.sechub.sharedkernel.Profiles;
 import com.daimler.sechub.sharedkernel.mapping.MappingData;
 
 /**
- * Contains additional rest call functionality for integration tests on scan domain
+ * Contains additional rest call functionality for integration tests on scan
+ * domain
  *
  * @author Albert Tregnaghi
  *
@@ -28,36 +35,67 @@ import com.daimler.sechub.sharedkernel.mapping.MappingData;
 @Profile(Profiles.INTEGRATIONTEST)
 public class IntegrationTestScanRestController {
 
-	@Autowired
-	private ScanAccessCountService scanAccessCountService;
+    @Autowired
+    private ScanAccessCountService scanAccessCountService;
 
-	@Autowired
-	private ProductResultCountService productResultCountService;
+    @Autowired
+    private ProductResultCountService productResultCountService;
 
-	@Autowired
-	private ScanReportCountService scanReportCountService;
+    @Autowired
+    private ScanReportCountService scanReportCountService;
 
-	@Autowired
+    @Autowired
     private UpdateScanMappingService updateScanMappingService;
 
-	@RequestMapping(path = APIConstants.API_ANONYMOUS + "integrationtest/project/{projectId}/scan/access/count", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
-	public long countProjectAccess(@PathVariable("projectId") String projectId) {
-		return scanAccessCountService.countProjectAccess(projectId);
-	}
+    @Autowired
+    private ScanMappingRepository scanMappingrepository;
+    
+    @Autowired
+    private ScanConfigService scanConfigService;
 
-	@RequestMapping(path = APIConstants.API_ANONYMOUS + "integrationtest/project/{projectId}/scan/report/count", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
-	public long countProductResults(@PathVariable("projectId") String projectId) {
-		return scanReportCountService.countProjectProductResults(projectId);
-	}
+    @RequestMapping(path = APIConstants.API_ANONYMOUS + "integrationtest/project/{projectId}/scan/access/count", method = RequestMethod.GET, produces = {
+            MediaType.APPLICATION_JSON_VALUE })
+    public long countProjectAccess(@PathVariable("projectId") String projectId) {
+        return scanAccessCountService.countProjectAccess(projectId);
+    }
 
-	@RequestMapping(path = APIConstants.API_ANONYMOUS + "integrationtest/project/{projectId}/scan/productresult/count", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
-	public long countScanResults(@PathVariable("projectId") String projectId) {
-		return productResultCountService.countProjectScanResults(projectId);
-	}
+    @RequestMapping(path = APIConstants.API_ANONYMOUS + "integrationtest/project/{projectId}/scan/report/count", method = RequestMethod.GET, produces = {
+            MediaType.APPLICATION_JSON_VALUE })
+    public long countProductResults(@PathVariable("projectId") String projectId) {
+        return scanReportCountService.countProjectProductResults(projectId);
+    }
 
-	@RequestMapping(path = APIConstants.API_ANONYMOUS + "integrationtest/config/scan/mapping/{mappingId}", method = RequestMethod.PUT)
-	public void updateScanMapping(@PathVariable("mappingId") String mappingId, @RequestBody MappingData mappingData) {
-	    updateScanMappingService.updateScanMapping(mappingId, mappingData);
-	}
+    @RequestMapping(path = APIConstants.API_ANONYMOUS + "integrationtest/project/{projectId}/scan/productresult/count", method = RequestMethod.GET, produces = {
+            MediaType.APPLICATION_JSON_VALUE })
+    public long countScanResults(@PathVariable("projectId") String projectId) {
+        return productResultCountService.countProjectScanResults(projectId);
+    }
+
+    @RequestMapping(path = APIConstants.API_ANONYMOUS + "integrationtest/config/scan/mapping/{mappingId}", method = RequestMethod.PUT)
+    public void updateScanMapping(@PathVariable("mappingId") String mappingId, @RequestBody MappingData mappingData) {
+        updateScanMappingService.updateScanMapping(mappingId, mappingData);
+    }
+
+    @RequestMapping(path = APIConstants.API_ANONYMOUS + "integrationtest/config/scan/mapping/{mappingId}", method = RequestMethod.GET)
+    public MappingData fetchScanMappingData(@PathVariable("mappingId") String mappingId) {
+	    Optional<ScanMapping> found = scanMappingrepository.findById(mappingId);
+	    if (found.isPresent()){
+	        MappingData data = MappingData.fromString(found.get().getData());
+	        return data;
+	    }
+	    return null;
+	            
+    }
+    @SuppressWarnings("deprecation")
+    @RequestMapping(path = APIConstants.API_ANONYMOUS + "integrationtest/config/namepattern/{namePatternProviderId}/{name}", method = RequestMethod.GET)
+    public String getIdForNameByProvider(@PathVariable("namePatternProviderId") String namePatternProviderId, @PathVariable("name") String name) {
+        NamePatternIdprovider provider = scanConfigService.getNamePatternIdProvider(namePatternProviderId);
+        if (provider==null) {
+            return null;
+        }
+        String id = provider.getIdForName(name);
+        return id;
+                
+    }
 
 }
