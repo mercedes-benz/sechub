2a44769689bd1e99927c4e14b574336887cb514a
Jeremias Eppler <jeremias.eppler@daimler.com>
2020-02-27 12:18:19 +0100

    Three button dialog added and used for adding URLs to whitelist, fixed cancel for description #158
    - added a new ThreeButtonDialog
    - added a ThreeButtonDialogResult class to store return value
    - uses ThreeButton dialog in create project action to be able to add
    more URLs to the whitelist
    - pressing the cancel button on the description field now terminates the
    project creation

--- sechub-developertools/src/main/java/com/daimler/sechub/developertools/admin/ui/action/project/CreateProjectAction.java
+++ sechub-developertools/src/main/java/com/daimler/sechub/developertools/admin/ui/action/project/CreateProjectAction.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.daimler.sechub.developertools.admin.ui.ThreeButtonDialogResult;
 import com.daimler.sechub.developertools.admin.ui.UIContext;
 import com.daimler.sechub.developertools.admin.ui.action.AbstractUIAction;
 import com.daimler.sechub.developertools.admin.ui.cache.InputCacheIdentifier;
@@ -19,31 +20,46 @@ public class CreateProjectAction extends AbstractUIAction {
 
 	@Override
 	public void execute(ActionEvent e) {
-		Optional<String> projectId = getUserInput("Please enter project ID/name",InputCacheIdentifier.PROJECT_ID);
-		if (! projectId.isPresent()) {
+		Optional<String> projectId = getUserInput("Please enter project ID/name", InputCacheIdentifier.PROJECT_ID);
+		if (!projectId.isPresent()) {
 			return;
 		}
-		Optional<String> description = getUserInput("Please enter a short description (optional)",null);
-		Optional<String> owner = getUserInput("Please enter owner user id (must exist)",InputCacheIdentifier.USERNAME);
-		if (! owner.isPresent()) {
+		
+		// in case the user does not input any value, the value will be an empty string. 
+		// Null will only be returned if the user pressed cancel.
+		Optional<String> description = getUserInput("Please enter a short description (optional)", null);
+		if (!description.isPresent()) {
+			return;
+		}
+
+		Optional<String> owner = getUserInput("Please enter owner user id (must exist)", InputCacheIdentifier.USERNAME);
+		if (!owner.isPresent()) {
 			return;
 		}
 
 		List<String> whiteListURLs = new ArrayList<>();
-		int i=1;
-		Optional<String>  uri;
+		int i = 1;
+		ThreeButtonDialogResult<String> uri;
 		do {
-			uri = getUserInput("(Optional) whitelist uri["+i+"]:",InputCacheIdentifier.WHITELIST_URI);
-			i++;
-			if (uri.isPresent()) {
-				whiteListURLs.add(uri.get());
+			uri = getUserInputFromField("(Optional) whitelist uri[" + i + "]:");
+
+			// stop if operation was canceled by the user
+			if (uri.isCanceled()) {
+				return;
 			}
-		}while(uri.isPresent());
 
+			if (uri.hasValue() && uri.isAdded()) {
+				// only increase counter if value is not empty
+				i++;
+				whiteListURLs.add(uri.getValue());
+			}
+		} while (!uri.isFinished()); // continue until finished is pressed
 
-		String postResult = getContext().getAdministration().createProject(projectId.get(),description.orElse(null), owner.get(),whiteListURLs);
+		// build and send request to server over HTTP
+		String postResult = getContext().getAdministration().createProject(projectId.get(), description.orElse(null),
+				owner.get(), whiteListURLs);
 		outputAsBeautifiedJSONOnSuccess(postResult);
-		outputAsTextOnSuccess("project created:"+projectId);
+		outputAsTextOnSuccess("project created:" + projectId);
 	}
 
 }
