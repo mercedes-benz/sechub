a82f01e0e5899496cf407b523fd01b6668fd23ae
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2020-05-27 16:26:09 +0200

    Fix failing tests #234
    - former change to ApiVersionValidationFactory let some test fail
      because wrong context configuration
    - fixed wrong result from mocked config service

--- sechub-scan/src/test/java/com/daimler/sechub/domain/scan/project/FalsePositiveJobServiceTest.java
+++ sechub-scan/src/test/java/com/daimler/sechub/domain/scan/project/FalsePositiveJobServiceTest.java
@@ -12,44 +12,49 @@ import com.daimler.sechub.sharedkernel.validation.ValidationResult;
 
 public class FalsePositiveJobServiceTest {
 
+    private static final String PROJECT_ID = "testprojectId";
     private FalsePositiveJobDataService serviceToTest;
     private FalsePositiveJobDataListValidation falsePositiveListValidation;
     private ScanProjectConfigService configService;
     private ScanAssertService scanAssertService;
     private UserInputAssertion userInputAssertion;
     private ScanProjectConfig config;
-    
+
     @Before
     public void before() {
         serviceToTest = new FalsePositiveJobDataService();
-        
+
         falsePositiveListValidation = mock(FalsePositiveJobDataListValidation.class);
-        configService=mock(ScanProjectConfigService.class);
-        userInputAssertion=mock(UserInputAssertion.class);
-        scanAssertService=mock(ScanAssertService.class);
-        
-        serviceToTest.falsePositiveJobDataListValidation=falsePositiveListValidation;
-        serviceToTest.configService=configService;
-        serviceToTest.scanAssertService=scanAssertService;
-        serviceToTest.userInputAssertion=userInputAssertion;
-        
+        configService = mock(ScanProjectConfigService.class);
+        userInputAssertion = mock(UserInputAssertion.class);
+        scanAssertService = mock(ScanAssertService.class);
+
+        serviceToTest.falsePositiveJobDataListValidation = falsePositiveListValidation;
+        serviceToTest.configService = configService;
+        serviceToTest.scanAssertService = scanAssertService;
+        serviceToTest.userInputAssertion = userInputAssertion;
+
         when(falsePositiveListValidation.validate(any(FalsePositiveJobDataList.class))).thenReturn(new ValidationResult());
+
+        /* we mock config service */
         config = new ScanProjectConfig();
-        when(configService.getOrCreate("project2",ScanProjectConfigID.FALSE_POSITIVE_CONFIGURATION, false, "{}")).thenReturn(config);
-        
+        config.setData("{}");
+
+        when(configService.getOrCreate(eq(PROJECT_ID), eq(ScanProjectConfigID.FALSE_POSITIVE_CONFIGURATION),eq(Boolean.FALSE),any())).thenReturn(config);
+
     }
-    
+
     @Test
     public void check_validations_are_triggered() {
         /* prepare */
         FalsePositiveJobDataList data = new FalsePositiveJobDataList();
-        
+
         /* execute */
-        serviceToTest.addFalsePositives("project2", data);
-        
+        serviceToTest.addFalsePositives(PROJECT_ID, data);
+
         /* test */
-        verify(userInputAssertion).isValidProjectId("project2");
-        verify(scanAssertService).assertUserHasAccessToProject("project2");
+        verify(userInputAssertion).isValidProjectId(PROJECT_ID);
+        verify(scanAssertService).assertUserHasAccessToProject(PROJECT_ID);
         verify(falsePositiveListValidation).validate(any(FalsePositiveJobDataList.class));
     }
 
