cf9e6dde75f70823b04c0d757edb9200f741e7b1
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2020-07-01 14:08:35 +0200

    PDS now build by bootJar with version from git #147

--- buildSrc/src/main/groovy/VersionData.groovy
+++ buildSrc/src/main/groovy/VersionData.groovy
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 class VersionData{
     /* we initialize with undefined */
-    static String productDelgationServerVersion = "undefined-serverversion"
+    static String pdsVersion = "undefined-serverversion"
     static String serverVersion = "undefined-serverversion"
     static String clientVersion = "undefined-clientversion"
     static String websiteVersion = "undefined-websiteversion"
