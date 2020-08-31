cb3b885889063b690744bb2a45ceb19074f2a4a5
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2020-08-31 18:23:33 +0200

    Changed SQL type to text fixes #351
    - changed type
    - added unit tests

---
+++ sechub-server/src/main/resources/db/migration/U8__enlarge_project_config_data_field.sql
@@ -0,0 +1,4 @@
+-- we switch back to old 4096 char constraint
+-- If this is not possible because already added bigger data, database shall give us an error, 
+-- so we know we have to migrate those data manually...
+ALTER TABLE scan_project_config ALTER COLUMN data varchar (4096) not null;
