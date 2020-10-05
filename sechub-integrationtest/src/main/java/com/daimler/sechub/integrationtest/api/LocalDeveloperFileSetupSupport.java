e0e83cadd903f5e07424e156081def0f543b4f0d
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2020-10-05 11:03:38 +0200

    Introducing ~/.sechub/sechub-developer.properties #148
    - it was very cumbersome to setup always
      `-Dsechub.integrationtest.running=true` for every eclipse launch
      configuration so now this can be done globally inside mentioned
      file. Is ignored in gradle builds

--- sechub-integrationtest/src/main/java/com/daimler/sechub/integrationtest/api/LocalDeveloperFileSetupSupport.java
+++ sechub-integrationtest/src/main/java/com/daimler/sechub/integrationtest/api/LocalDeveloperFileSetupSupport.java
@@ -9,12 +9,11 @@ import org.slf4j.LoggerFactory;
 
 /**
  * A little helper class for developers: it is very inconvenient to setup some
- * standard when doing a local development and you have to turn on every junit
- * test launch with -Dsechub.integrationtest.running=true (otherwise tests will
- * be skipped). To simpify this this class was created. Just create file
- * ~/.sechub/sechub-developer.properties and set their your settings. But be
- * aware of local running gradle build - integration tests will always run as
- * well then!
+ * standard when doing a local development in your IDE and you have to turn on
+ * every junit test launch with -Dsechub.integrationtest.running=true (otherwise
+ * tests will be skipped). To simplify this, this class was created. Just create
+ * file `~/.sechub/sechub-developer.properties` and set there your settings. At
+ * Gradle builds this will be ignored. well then!
  * 
  * @author Albert Tregnaghi
  *
@@ -31,10 +30,13 @@ public class LocalDeveloperFileSetupSupport {
         File userHome = new File(System.getProperty("user.home"));
         File sechubHidden = new File(userHome, ".sechub");
         File sechubDevConfig = new File(sechubHidden, "sechub-developer.properties");
-        /*
-         * TODO Albert Tregnaghi, 2020-10-02: it would be nice to check a variable only
-         * availabel at gradle calls, so this would not work on gradle builds
-         */
+
+        String buildGradleEnv = System.getenv("SECHUB_BUILD_GRADLE");
+        if (Boolean.parseBoolean(buildGradleEnv)) {
+            LOG.info("Recognized gradle build, skip check for :{}", sechubDevConfig);
+            return;
+        }
+
         if (!sechubDevConfig.exists()) {
             return;
         }
