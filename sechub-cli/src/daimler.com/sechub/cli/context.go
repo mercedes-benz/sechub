617b7b8207d16bc25d2b9b3a93e2631c2e7d62a1
Sven Dolderer <sven.dolderer@daimler.com>
2020-10-05 17:21:08 +0200

    added proxy support for client #322
    - read proxy settings from environment
    - added documentation of proxy support

--- sechub-cli/src/daimler.com/sechub/cli/context.go
+++ sechub-cli/src/daimler.com/sechub/cli/context.go
@@ -10,13 +10,13 @@ import (
 
 // Context - represents global context of this sechub client call. Structure contains all relevant data for SecHub client functionality (configuration, states, client and more
 type Context struct {
-	config                *Config
-	inputForContentProcessing     []byte // used for log output and as base for templating, no data replaced (so apitoken env variable is still there as variable and suitable for logging)
+	config                    *Config
+	inputForContentProcessing []byte // used for log output and as base for templating, no data replaced (so apitoken env variable is still there as variable and suitable for logging)
 	contentToSend             []byte // template output used for communication to server - contains replaced parts from env variables (e.g. password instead of variable)
-	HTTPClient            *http.Client
-	sechubConfig          *SecHubConfig
-	sourceZipFileChecksum string
-	sourceZipFileName     string
+	HTTPClient                *http.Client
+	sechubConfig              *SecHubConfig
+	sourceZipFileChecksum     string
+	sourceZipFileName         string
 }
 
 func (context *Context) isUploadingSourceZip() bool {
@@ -31,6 +31,7 @@ func NewContext(config *Config) *Context {
 	/* setup HTTP client */
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: config.trustAll},
+		Proxy:           http.ProxyFromEnvironment,
 	}
 	context.HTTPClient = &http.Client{Timeout: time.Duration(context.config.timeOutNanoseconds), Transport: tr}
 
