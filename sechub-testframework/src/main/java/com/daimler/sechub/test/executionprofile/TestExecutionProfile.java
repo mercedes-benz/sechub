38508eb86c19fcff52e8aefc3a97e13a63dfb6ec
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2020-10-14 18:20:07 +0200

    Review change #391 - removed unnecessary annoatation

--- sechub-testframework/src/main/java/com/daimler/sechub/test/executionprofile/TestExecutionProfile.java
+++ sechub-testframework/src/main/java/com/daimler/sechub/test/executionprofile/TestExecutionProfile.java
@@ -6,14 +6,11 @@ import java.util.Set;
 
 import com.daimler.sechub.test.executorconfig.TestExecutorConfig;
 
-import wiremock.com.fasterxml.jackson.annotation.JsonInclude;
-import wiremock.com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 public class TestExecutionProfile {
 
     public String id;
     
-    @JsonInclude(Include.NON_NULL)
     public String description;
     
     public Set<TestExecutorConfig> configurations = new HashSet<>();
