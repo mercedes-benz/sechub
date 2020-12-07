c41eb1eef00f40afced471f166b9225b47385108
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2020-12-07 19:09:16 +0100

    Adding hashCode() and equals() for ProjectMetaDataEntityId clazz
    - clazz is a serializable and Spring Boot is complaining about not
      having both methods implemented (comes in log at startup)
    - while doing #472 we found this one and fixed it

--- sechub-administration/src/main/java/com/daimler/sechub/domain/administration/project/ProjectMetaDataEntityId.java
+++ sechub-administration/src/main/java/com/daimler/sechub/domain/administration/project/ProjectMetaDataEntityId.java
@@ -1,6 +1,7 @@
 package com.daimler.sechub.domain.administration.project;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class ProjectMetaDataEntityId implements Serializable {
 	
@@ -8,5 +9,22 @@ public class ProjectMetaDataEntityId implements Serializable {
 	
 	String projectId;
 	String key;
+	
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, projectId);
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ProjectMetaDataEntityId other = (ProjectMetaDataEntityId) obj;
+        return Objects.equals(key, other.key) && Objects.equals(projectId, other.projectId);
+    }
 
 }
