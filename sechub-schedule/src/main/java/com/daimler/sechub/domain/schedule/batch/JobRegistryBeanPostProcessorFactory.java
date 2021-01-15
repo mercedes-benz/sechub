eafa91787ab346bb7e5ef86d9b10aea48078720f
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2021-01-15 15:34:35 +0100

    Add a job regstiry bean post processor factory #499
    - avoid problem of secondary bug
      "No job configuration with the name [executeScan] was registered"
      by providing a JobRegistryBeanPostProcessor by a factory component

---
+++ sechub-schedule/src/main/java/com/daimler/sechub/domain/schedule/batch/JobRegistryBeanPostProcessorFactory.java
@@ -0,0 +1,21 @@
+package com.daimler.sechub.domain.schedule.batch;
+
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobRegistryBeanPostProcessorFactory {
+
+    @Autowired
+    JobRegistry jobRegistry;
+
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor() {
+        JobRegistryBeanPostProcessor postProcessor = new JobRegistryBeanPostProcessor();
+        postProcessor.setJobRegistry(jobRegistry);
+        return postProcessor;
+    }
+}
