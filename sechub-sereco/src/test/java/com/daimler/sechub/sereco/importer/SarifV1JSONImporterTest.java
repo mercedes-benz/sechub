93c0b955052164cb3e6a8c14a41f5a58090ad626
Weissert, Albert (415) <albert.weissert@daimler.com>
2021-02-24 14:44:36 +0100

    #386: added UT for threadflows

--- sechub-sereco/src/test/java/com/daimler/sechub/sereco/importer/SarifV1JSONImporterTest.java
+++ sechub-sereco/src/test/java/com/daimler/sechub/sereco/importer/SarifV1JSONImporterTest.java
@@ -21,25 +21,30 @@ public class SarifV1JSONImporterTest {
     private SarifV1JSONImporter importerToTest;
     private String sarifBrakeman;
     private String sarifEmptyResult;
+    private String sarifThreadflowsExample;
     
     @Before
     public void before() {
         importerToTest = new SarifV1JSONImporter();
         sarifBrakeman = SerecoTestFileSupport.INSTANCE.loadTestFile("sarif/sarif_2.1.0_brakeman.json");
-        sarifEmptyResult = SerecoTestFileSupport.INSTANCE.loadTestFile("sarif/sarif_2.1.0_empty_results.json");    
+        sarifEmptyResult = SerecoTestFileSupport.INSTANCE.loadTestFile("sarif/sarif_2.1.0_empty_results.json");
+        sarifThreadflowsExample = SerecoTestFileSupport.INSTANCE.loadTestFile("sarif/sarif_2.1.0_threadflows_example.json");
     }
 
     @Test
     public void sarifreport_can_be_imported() {
         /* prepare */
 
-        ImportParameter param = ImportParameter.builder().importData(sarifBrakeman).importId("id1").productId("SARIF").build();
+        ImportParameter paramBrakeman = ImportParameter.builder().importData(sarifBrakeman).importId("id1").productId("SARIF").build();
+        ImportParameter paramThreadFlows = ImportParameter.builder().importData(sarifThreadflowsExample).importId("id1").productId("SARIF").build();
 
         /* execute */
-        ProductImportAbility ableToImport = importerToTest.isAbleToImportForProduct(param);
+        ProductImportAbility ableToImportBrakemanSarif = importerToTest.isAbleToImportForProduct(paramBrakeman);
+        ProductImportAbility ableToImportThreadFlowSarif = importerToTest.isAbleToImportForProduct(paramThreadFlows);
 
         /* test */
-        assertEquals("Was able to import sarif!", ProductImportAbility.ABLE_TO_IMPORT, ableToImport);
+        assertEquals("Was able to import sarif!", ProductImportAbility.ABLE_TO_IMPORT, ableToImportBrakemanSarif);
+        assertEquals("Was able to import sarif!", ProductImportAbility.ABLE_TO_IMPORT, ableToImportThreadFlowSarif);
     }
 
     @Test
@@ -110,7 +115,28 @@ public class SarifV1JSONImporterTest {
         assertEquals("Gemfile.lock", codeInfo.getLocation());
         assertEquals(115, codeInfo.getLine().intValue());
         assertEquals(21, vulnerabilities.size());
+    }
+    
+    @Test
+    public void sarifreport_threadflow_locations() throws Exception {
+        /* prepare */
+        SerecoMetaData result = importerToTest.importResult(sarifThreadflowsExample);
 
+        /* execute */
+        List<SerecoVulnerability> vulnerabilities = result.getVulnerabilities();
+        SerecoVulnerability v1 = fetchFirstNonFalsePositive(vulnerabilities);
+        SerecoCodeCallStackElement codeInfo = v1.getCode();
+
+        /* test */
+        assertNotNull(codeInfo);
+        assertEquals("3-Beyond-basics/bad-eval-with-code-flow.py", codeInfo.getLocation());
+        assertEquals(3, codeInfo.getLine().intValue());
+        assertEquals(1, vulnerabilities.size());
+        
+        SerecoCodeCallStackElement subCodeInfo = codeInfo.getCalls();
+        assertNotNull(subCodeInfo);
+        assertEquals("3-Beyond-basics/bad-eval-with-code-flow.py", subCodeInfo.getLocation());
+        assertEquals(4, subCodeInfo.getLine().intValue());
     }
 
     private SerecoVulnerability fetchFirstNonFalsePositive(List<SerecoVulnerability> vulnerabilities) {
