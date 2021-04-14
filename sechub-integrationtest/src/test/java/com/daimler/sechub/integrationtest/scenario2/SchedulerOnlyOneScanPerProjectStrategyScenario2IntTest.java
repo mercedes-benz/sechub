8efc3f3864fb6040ad3e2faf1aefcb20cd39f57d
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2021-04-14 16:39:21 +0200

    Added new test to show problem, next build will fail #549
    - strategy "only-one-scan-per-project-at-a-time" was not really
      tested by current integration test
    - reason: we have different JVMs inside integration tests,
      so spring testresources annotation does not work
    - the test will fail next build, applicaiton-integrationtest.yml
      having
      sechub.scheduler.strategy.id="only-one-scan-per-project-at-a-time"
      will make this specific test green
    - the next step is to provide a possiblitiy for integration tests to
      change the strategy at runtime (those changes should be done inside
      IntegrationTestSchedulerRestController.java)

--- sechub-integrationtest/src/test/java/com/daimler/sechub/integrationtest/scenario2/SchedulerOnlyOneScanPerProjectStrategyScenario2IntTest.java
+++ sechub-integrationtest/src/test/java/com/daimler/sechub/integrationtest/scenario2/SchedulerOnlyOneScanPerProjectStrategyScenario2IntTest.java
@@ -3,6 +3,7 @@ package com.daimler.sechub.integrationtest.scenario2;
 
 import static com.daimler.sechub.integrationtest.api.TestAPI.*;
 import static com.daimler.sechub.integrationtest.scenario2.Scenario2.*;
+import static com.daimler.sechub.integrationtest.api.AssertJob.*;
 import java.util.UUID;
 import org.junit.Rule;
 import org.junit.Test;
@@ -129,4 +130,40 @@ public class SchedulerOnlyOneScanPerProjectStrategyScenario2IntTest {
         /* @formatter:on */
     }
 
+    @Test
+    public void project1_job1_project1_job2_project2_job3__job3_is_executed_before_job2() {
+        /* @formatter:off */
+        
+        /* prepare */
+                
+        as(SUPER_ADMIN).
+            assignUserToProject(USER_1, PROJECT_1);
+        
+        as(SUPER_ADMIN).
+            assignUserToProject(USER_1, PROJECT_2);
+        
+        /* execute */
+        UUID project1job1 = as(USER_1).triggerAsyncCodeScanWithPseudoZipUpload(PROJECT_1, IntegrationTestMockMode.CODE_SCAN__CHECKMARX__GREEN__LONG_RUNNING);
+        UUID project1job2 = as(USER_1).triggerAsyncCodeScanWithPseudoZipUpload(PROJECT_1, IntegrationTestMockMode.CODE_SCAN__CHECKMARX__GREEN__LONG_RUNNING);                
+        UUID project2job3 = as(USER_1).triggerAsyncCodeScanWithPseudoZipUpload(PROJECT_2, IntegrationTestMockMode.CODE_SCAN__CHECKMARX__GREEN__LONG_RUNNING);
+        
+        /* test */
+        // check job1 is running
+        waitForJobRunning(PROJECT_1, project1job1);
+        assertJobIsRunning(PROJECT_1, project1job1);
+        assertJobHasNotRun(PROJECT_1, project1job2);
+        
+        // check job 3 and job1 are running but not job2 
+        waitForJobRunning(PROJECT_2, project2job3);
+        assertJobIsRunning(PROJECT_2, project2job3);
+        assertJobIsRunning(PROJECT_1, project1job1);
+        
+        assertJobHasNotRun(PROJECT_1, project1job2); // not this job
+        
+        // check job 3 is executed when job1 has finished
+        waitForJobDone(PROJECT_1, project1job1);
+        waitForJobDone(PROJECT_1, project1job2); // wait job3 has been done as well
+        
+        /* @formatter:on */
+    }
 }
