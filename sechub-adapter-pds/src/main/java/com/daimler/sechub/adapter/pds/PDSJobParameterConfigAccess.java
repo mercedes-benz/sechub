15eef328a9888945bec3ad8ea277a7b7ed3be540
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2021-05-28 23:41:38 +0200

    Added missing SPDX headers #435

--- sechub-adapter-pds/src/main/java/com/daimler/sechub/adapter/pds/PDSJobParameterConfigAccess.java
+++ sechub-adapter-pds/src/main/java/com/daimler/sechub/adapter/pds/PDSJobParameterConfigAccess.java
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 package com.daimler.sechub.adapter.pds;
 
 import java.util.Collections;
--- sechub-pds-core/src/main/java/com/daimler/sechub/pds/job/PDSJobConfigurationSupport.java
+++ sechub-pds-core/src/main/java/com/daimler/sechub/pds/job/PDSJobConfigurationSupport.java
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 package com.daimler.sechub.pds.job;
 
 import com.daimler.sechub.pds.execution.PDSExecutionParameterEntry;
