cfec0397d6ad8b3b7b8885e99c5e6a7033f33c79
Jeremias Eppler <jeremias.eppler@daimler.com>
2021-06-07 17:12:22 +0200

    publish api client to local maven repo #377

--- sechub-client-java/generateClient.sh
+++ sechub-client-java/generateClient.sh
@@ -4,5 +4,11 @@
 
 genFolder="$1"
 
+# Make gradlew executable
 chmod +x "$genFolder/gradlew"
+
+# Build Java Client
 "$genFolder/gradlew" --project-dir "$genFolder" build
+
+# Publish artifacts to local maven repository
+"$genFolder/gradlew" --project-dir "$genFolder" install
