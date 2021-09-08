157a899eb71848bc3f613755f0e0e50f7788ed20
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2021-09-08 16:59:56 +0200

    Enhanced PDFileUnzipSupport, fixes #773
    - the uploaded zip file did not contain directory entries but only
      file information with path meta data. So the old implementation
      did not create missing directories and did fail.
    - added test zip file with
    - wrote test reading test zip file and reproduce failure before fixing

--- sechub-pds/src/main/java/com/daimler/sechub/pds/util/PDSFileUnzipSupport.java
+++ sechub-pds/src/main/java/com/daimler/sechub/pds/util/PDSFileUnzipSupport.java
@@ -45,47 +45,80 @@ public class PDSFileUnzipSupport {
     }
 
     public UnzipResult unzipArchive(File file, File destDir) throws IOException {
-        UnzipResult result = new UnzipResult();
+        UnzipResult unzipResult = new UnzipResult();
         if (!file.exists()) {
             LOG.error("cannot unzip {} because zip file does not exist!", file.getAbsolutePath());
-            return result;
+            return unzipResult;
         }
         if (!destDir.exists()) {
             destDir.mkdirs();
         }
-        result.targetLocation = destDir.getAbsolutePath();
-        result.sourceLocation = file.getAbsolutePath();
+        unzipResult.targetLocation = destDir.getAbsolutePath();
+        unzipResult.sourceLocation = file.getAbsolutePath();
 
-        LOG.debug("start unzipping of {} into {}", result.sourceLocation, result.targetLocation);
+        LOG.debug("start unzipping of {} into {}", unzipResult.sourceLocation, unzipResult.targetLocation);
 
         try (ZipInputStream zis = new ZipInputStream(new FileInputStream(file))) {
 
             ZipEntry zipEntry = zis.getNextEntry();
             while (zipEntry != null) {
-                File newFile = newFile(result, destDir, zipEntry);
-                copy(zis, newFile);
+
+                File newFile = newFile(unzipResult, destDir, zipEntry);
+
+                copy(zis, newFile, unzipResult);
                 zipEntry = zis.getNextEntry();
             }
             zis.closeEntry();
         }
-        return result;
+        return unzipResult;
     }
 
-    private void copy(ZipInputStream zis, File newFile) throws FileNotFoundException, IOException {
+    private void copy(ZipInputStream zis, File newFile, UnzipResult unzipResult) throws FileNotFoundException, IOException {
         LOG.trace("Handle", newFile);
 
         if (newFile.isDirectory()) {
-            /* we do not copy directory streams... */
+            /* we do not copy directory entries */
             LOG.trace("Skipped, because directory:{}", newFile);
             return;
         }
 
         /* create/copy file from zip content */
+        ensureParentFoldersCreated(newFile, unzipResult);
+
         try (FileOutputStream fos = new FileOutputStream(newFile)) {
             LOG.trace("Create:{}", newFile);
 
             IOUtils.copy(zis, fos);
         }
+    }
+
+    private void ensureParentFoldersCreated(File newFile, UnzipResult unzipResult) throws IOException {
+        File parentFile = newFile.getParentFile();
+        if (parentFile == null) {
+            throw new IOException("Parent directory 'null' not acceptable, but was set for new file:" + newFile.getAbsolutePath());
+        }
+        if (parentFile.exists()) {
+            return;
+        }
+        /*
+         * Parent file does not exist/was not created before. Means the ZIP file does
+         * not contain directory entries but only file entries with path info, so we
+         * must create missing parent folders:
+         */
+        int countOfMissingDirectories = 0;
+        File missing = parentFile;
+        while (missing != null && !missing.exists()) {
+            countOfMissingDirectories++;
+            missing = missing.getParentFile();
+        }
+
+        LOG.trace("Will create {} missing directories for parent file:{}", countOfMissingDirectories, parentFile.getAbsolutePath());
+
+        if (!parentFile.mkdirs()) {
+            throw new IOException("Was not able to create directories for parent folder:" + parentFile.getAbsolutePath());
+        }
+
+        unzipResult.createdFoldersCount += countOfMissingDirectories;
     }
 
     private File newFile(UnzipResult unzipResult, File destinationDir, ZipEntry zipEntry) throws IOException {
