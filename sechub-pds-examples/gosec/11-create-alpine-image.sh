d54804cba98147259e8a3a57da24014d5810f481
Jeremias Eppler <jeremias.eppler@daimler.com>
2021-09-10 22:02:27 +0200

    add pull to build commands #791

--- sechub-pds-examples/gosec/11-create-alpine-image.sh
+++ sechub-pds-examples/gosec/11-create-alpine-image.sh
@@ -23,6 +23,6 @@ then
 fi
 
 echo ">> Base image: $BASE_IMAGE"
-docker build --no-cache --build-arg BASE_IMAGE=$BASE_IMAGE --tag "$REGISTRY:$VERSION" --file docker/GoSec-Alpine.dockerfile docker/
+docker build --pull --no-cache --build-arg BASE_IMAGE=$BASE_IMAGE --tag "$REGISTRY:$VERSION" --file docker/GoSec-Alpine.dockerfile docker/
 docker tag "$REGISTRY:$VERSION" "$REGISTRY:latest"
 
