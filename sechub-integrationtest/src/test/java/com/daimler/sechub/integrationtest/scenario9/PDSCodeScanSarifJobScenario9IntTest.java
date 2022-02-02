0a1c085980e597a008d7e470516b04b649781ac2
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2022-02-02 16:29:27 +0100

    After changes brakeman description text used - fixed test #606
    - after the changes on SARIF handling the brakeman result
      descriptions are used now instead of the generic rule
      descriptions. So the test failed
    - The result description are superior to the generic ones, so
      the handling is now better
    - Adopted test

--- sechub-integrationtest/src/test/java/com/daimler/sechub/integrationtest/scenario9/PDSCodeScanSarifJobScenario9IntTest.java
+++ sechub-integrationtest/src/test/java/com/daimler/sechub/integrationtest/scenario9/PDSCodeScanSarifJobScenario9IntTest.java
@@ -76,7 +76,7 @@ public class PDSCodeScanSarifJobScenario9IntTest {
                    hasSeverity(Severity.HIGH).
                    hasScanType(ScanType.CODE_SCAN).
                    hasName("BRAKE0102").
-                   hasDescription("Checks for XSS in calls to content_tag.").
+                   hasDescription("Rails 5.0.0 `content_tag` does not escape double quotes in attribute values (CVE-2016-6316). Upgrade to Rails 5.0.0.1.").
                    codeCall(0).
                       hasLocation("Gemfile.lock").
                       hasLine(115).
@@ -84,7 +84,7 @@ public class PDSCodeScanSarifJobScenario9IntTest {
                    hasName("BRAKE0116").
                    hasScanType(ScanType.CODE_SCAN).
                    hasSeverity(Severity.MEDIUM).
-                   hasDescription("Checks for versions with CSRF token forgery vulnerability (CVE-2020-8166).");
+                   hasDescription("Rails 5.0.0 has a vulnerability that may allow CSRF token forgery. Upgrade to Rails 5.2.4.3 or patch.");
         
         /* @formatter:on */
     }
