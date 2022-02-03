e08b591a0c5ac9b030864b34c1532cf76b9a6aba
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2022-02-03 10:51:30 +0100

    Improved implementation and testing of fp strategy for web scans #606

--- sechub-scan-product-sereco/src/main/java/com/daimler/sechub/domain/scan/product/sereco/SerecoFalsePositiveWebScanStrategy.java
+++ sechub-scan-product-sereco/src/main/java/com/daimler/sechub/domain/scan/product/sereco/SerecoFalsePositiveWebScanStrategy.java
@@ -9,11 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.daimler.sechub.commons.model.ScanType;
-import com.daimler.sechub.domain.scan.project.FalsePositiveCodeMetaData;
-import com.daimler.sechub.domain.scan.project.FalsePositiveCodePartMetaData;
 import com.daimler.sechub.domain.scan.project.FalsePositiveMetaData;
+import com.daimler.sechub.domain.scan.project.FalsePositiveWebMetaData;
 import com.daimler.sechub.sereco.metadata.SerecoClassification;
-import com.daimler.sechub.sereco.metadata.SerecoCodeCallStackElement;
 import com.daimler.sechub.sereco.metadata.SerecoVulnerability;
 
 /**
@@ -46,9 +44,9 @@ public class SerecoFalsePositiveWebScanStrategy {
             return false;
         }
 
-        FalsePositiveCodeMetaData metaDataCode = metaData.getCode();
-        if (metaDataCode == null) {
-            LOG.error("Cannot check code vulnerability for false positives when meta data has no web parts!");
+        FalsePositiveWebMetaData metaDataWeb = metaData.getWeb();
+        if (metaDataWeb == null) {
+            LOG.error("Cannot check web vulnerability for false positives when meta data has no web parts!");
             return false;
         }
 
@@ -56,10 +54,10 @@ public class SerecoFalsePositiveWebScanStrategy {
         /* -------------------CWE ID--------------------------- */
         /* ---------------------------------------------------- */
 
-        /* for code scans we only use CWE as wellknown common identifier */
+        /* for web scans we only use CWE as wellknown common identifier */
         Integer cweId = metaData.getCweId();
         if (cweId == null) {
-            LOG.error("Cannot check code vulnerability for false positives when code meta data has no CWE id set!");
+            LOG.error("Cannot check web vulnerability for false positives web code meta data has no CWE id set!");
             return false;
         }
 
@@ -83,98 +81,7 @@ public class SerecoFalsePositiveWebScanStrategy {
 
         }
 
-        /* ------------------------------------------------------- */
-        /* -------------------Location---------------------------- */
-        /* ------------------------------------------------------- */
-        SerecoCodeCallStackElement serecoFirstElement = vulnerability.getCode();
-        if (serecoFirstElement == null) {
-            /* strange - canot be investigated */
-            LOG.warn("Cannot check code vulnerability for false positives when no first code element is found!");
-            return false;
-        }
-        FalsePositiveCodePartMetaData start = metaDataCode.getStart();
-        if (start == null) {
-            LOG.warn("Cannot check code vulnerability for false positives when no start code meta data is found!");
-            return false;
-        }
-        if (isLocationDifferent(start, serecoFirstElement)) {
-            return false;
-        }
-        FalsePositiveCodePartMetaData end = metaDataCode.getEnd();
-        SerecoCodeCallStackElement serecoLastElement = findLastElement(serecoFirstElement);
-        if (isLocationDifferent(end, serecoLastElement)) {
-            return false;
-        }
-        /* ------------------------------------------------------- */
-        /* -------------------Relevant parts---------------------- */
-        /* ------------------------------------------------------- */
-        String relevant1 = start.getRelevantPart();
-        String relevant2 = serecoFirstElement.getRelevantPart();
-
-        if (relevant1 == null || relevant1.isEmpty()) {
-            relevant1 = createRelevantReplacment(start);
-        }
-        if (relevant2 == null || relevant2.isEmpty()) {
-            relevant2 = createRelevantReplacment(serecoFirstElement);
-        }
-        if (!relevant1.equals(relevant2)) {
-            return false;
-        }
-        String relevant3 = "";
-        String relevant4 = "";
-        if (end != null) {
-            relevant3 = end.getRelevantPart();
-            if (relevant3 == null || relevant3.isEmpty()) {
-                relevant3 = createRelevantReplacment(end);
-            }
-        }
-        if (serecoLastElement != null) {
-            relevant4 = serecoLastElement.getRelevantPart();
-            if (relevant4 == null || relevant4.isEmpty()) {
-                relevant4 = createRelevantReplacment(serecoLastElement);
-            }
-        }
-        if (!relevant3.equals(relevant4)) {
-            return false;
-        }
         return true;
-    }
-
-    private String createRelevantReplacment(SerecoCodeCallStackElement serecoFirstElement) {
-        return relevantPartResolver.toRelevantPart(serecoFirstElement.getSource());
-    }
-
-    private String createRelevantReplacment(FalsePositiveCodePartMetaData metaData) {
-        return relevantPartResolver.toRelevantPart(metaData.getSourceCode());
-    }
-
-    private boolean isLocationDifferent(FalsePositiveCodePartMetaData metaData, SerecoCodeCallStackElement serecoElement) {
-        if (metaData == null && serecoElement == null) {
-            return false;
-        }
-        if (metaData == null) {
-            return true;
-        }
-        if (serecoElement == null) {
-            return true;
-        }
-        String location1 = metaData.getLocation();
-        String location2 = serecoElement.getLocation();
-
-        if (location1 == null || location2 == null) {
-            LOG.warn("at least one location is null! should not be at this stage. location1:{}, location1:{}", location1, location2);
-            return true; // we do not accept this at all
-        }
-        return !location2.equals(location1);
-    }
-
-    private SerecoCodeCallStackElement findLastElement(SerecoCodeCallStackElement serecoFirstElement) {
-        SerecoCodeCallStackElement calls = serecoFirstElement;
-        do {
-            calls = calls.getCalls();
-        } while (calls != null && calls.getCalls() != null);
-
-        return calls;
     }
 
 }
--- sechub-scan-product-sereco/src/test/java/com/daimler/sechub/domain/scan/product/sereco/SerecoFalsePositiveWebScanStrategyTest.java
+++ sechub-scan-product-sereco/src/test/java/com/daimler/sechub/domain/scan/product/sereco/SerecoFalsePositiveWebScanStrategyTest.java
@@ -3,15 +3,22 @@ package com.daimler.sechub.domain.scan.product.sereco;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.NullSource;
 
+import com.daimler.sechub.commons.model.ScanType;
 import com.daimler.sechub.domain.scan.project.FalsePositiveMetaData;
+import com.daimler.sechub.domain.scan.project.FalsePositiveWebMetaData;
 import com.daimler.sechub.sereco.metadata.SerecoVulnerability;
 import com.daimler.sechub.sereco.metadata.SerecoWeb;
 import com.daimler.sechub.sereco.metadata.SerecoWebRequest;
 
 class SerecoFalsePositiveWebScanStrategyTest {
 
+    private static final int CWE_ID_4711 = 4711;
     private SerecoFalsePositiveWebScanStrategy strategyToTest;
 
     @BeforeEach
@@ -19,13 +26,15 @@ class SerecoFalsePositiveWebScanStrategyTest {
         strategyToTest = new SerecoFalsePositiveWebScanStrategy();
     }
 
+    @DisplayName("Not false positive. Nearly valid false positive, but other cweId in meta data")
     @Test
-    void same_cwe_id_nothing_more_set_both_is_false_positive() {
+    void no_false_positive_because_missing_data1() {
         /* prepare */
-        FalsePositiveMetaData metaData = createTestFalsePositiveMetaData();
-        
-        SerecoVulnerability vulnerability = createTestVulnerability();
-        
+        FalsePositiveMetaData metaData = createValidTestFalsePositiveMetaData();
+        metaData.setCweId(CWE_ID_4711 + 1);
+
+        SerecoVulnerability vulnerability = createValidTestVulnerability();
+
         /* execute */
         boolean isFalsePositive = strategyToTest.isFalsePositive(vulnerability, metaData);
 
@@ -33,17 +42,96 @@ class SerecoFalsePositiveWebScanStrategyTest {
         assertFalse(isFalsePositive);
     }
 
-    private FalsePositiveMetaData createTestFalsePositiveMetaData() {
+    @DisplayName("Not false positive. Nearly valid false positive, but cweId in vulnerability is defined as 'other'")
+    @Test
+    void no_false_positive_because_missing_data2() {
+        /* prepare */
+        FalsePositiveMetaData metaData = createValidTestFalsePositiveMetaData();
+
+        SerecoVulnerability vulnerability = createValidTestVulnerability();
+        vulnerability.getClassification().setCwe("other");
+
+        /* execute */
+        boolean isFalsePositive = strategyToTest.isFalsePositive(vulnerability, metaData);
+
+        /* test */
+        assertFalse(isFalsePositive);
+    }
+
+    @DisplayName("Not false positive. Same CWE id in vulnerability and metadata defined. Scan type set, but no web metata available. Must result in NO fp")
+    @Test
+    void no_false_positive_because_missing_data3() {
+        /* prepare */
         FalsePositiveMetaData metaData = new FalsePositiveMetaData();
         metaData.setCweId(4711);
+        metaData.setWeb(new FalsePositiveWebMetaData());
+
+        SerecoVulnerability vulnerability = new SerecoVulnerability();
+        vulnerability.getClassification().setCwe("4711");
+        vulnerability.setScanType(ScanType.WEB_SCAN);
+
+        /* execute */
+        boolean isFalsePositive = strategyToTest.isFalsePositive(vulnerability, metaData);
+
+        /* test */
+        assertFalse(isFalsePositive);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = ScanType.class, names = "WEB_SCAN", mode = EnumSource.Mode.EXCLUDE)
+    @DisplayName("Not false positive. Nearly valid false positive, but not marked because wrong scan type in meta data defined")
+    @NullSource
+    void no_false_positive_because_wrong_scan_type(ScanType type) {
+        /* prepare */
+        FalsePositiveMetaData metaData = createValidTestFalsePositiveMetaData();
+        SerecoVulnerability vulnerability = createValidTestVulnerability();
+
+        vulnerability.setScanType(type);// but we change scan type...
+
+        /* execute */
+        boolean isFalsePositive = strategyToTest.isFalsePositive(vulnerability, metaData);
+
+        /* test */
+        assertFalse(isFalsePositive);// no longer found
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = ScanType.class, names = "WEB_SCAN", mode = EnumSource.Mode.INCLUDE)
+    @DisplayName("IS false positive. Valid false positive defined, scan type is correct")
+    void is_false_positive_because_correct_scan_type(ScanType type) {
+        /* prepare */
+        FalsePositiveMetaData metaData = createValidTestFalsePositiveMetaData();
+        SerecoVulnerability vulnerability = createValidTestVulnerability();
+
+        vulnerability.setScanType(type);// we change scan type...(if not already set)
+
+        /* execute */
+        boolean isFalsePositive = strategyToTest.isFalsePositive(vulnerability, metaData);
+
+        /* test */
+        assertTrue(isFalsePositive);// no longer found
+    }
+
+    /* ++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    /* + ..................Helpers....................... + */
+    /* ++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    private FalsePositiveMetaData createValidTestFalsePositiveMetaData() {
+        FalsePositiveMetaData metaData = new FalsePositiveMetaData();
+        metaData.setCweId(4711);
+        metaData.setScanType(ScanType.WEB_SCAN);
+        FalsePositiveWebMetaData web = new FalsePositiveWebMetaData();
+        metaData.setWeb(web);
         return metaData;
     }
 
-    private SerecoVulnerability createTestVulnerability() {
+    private SerecoVulnerability createValidTestVulnerability() {
         SerecoVulnerability vulnerability = new SerecoVulnerability();
         SerecoWeb web = new SerecoWeb();
+        vulnerability.getClassification().setCwe(""+CWE_ID_4711);
         vulnerability.setWeb(web);
+        vulnerability.setScanType(ScanType.WEB_SCAN);
         
+
         SerecoWebRequest request = web.getRequest();
         request.setMethod("method");
         request.setTarget("target");
@@ -51,7 +139,7 @@ class SerecoFalsePositiveWebScanStrategyTest {
         request.setVersion("version");
 
         web.getResponse().setStatusCode(3333);
-        
+
         return vulnerability;
     }
 
