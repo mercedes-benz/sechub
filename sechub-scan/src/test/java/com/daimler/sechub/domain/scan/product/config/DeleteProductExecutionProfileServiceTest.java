7e0df2911e3ea040ce4b015eab84681365381387
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2022-02-16 07:04:40 +0100

    Review changes #767
    - changed junit4 test to junit5
    - improved both test cases

--- sechub-scan/src/test/java/com/daimler/sechub/domain/scan/product/config/DeleteProductExecutionProfileServiceTest.java
+++ sechub-scan/src/test/java/com/daimler/sechub/domain/scan/product/config/DeleteProductExecutionProfileServiceTest.java
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
 package com.daimler.sechub.domain.scan.product.config;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 import java.util.Optional;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import com.daimler.sechub.sharedkernel.error.NotFoundException;
 import com.daimler.sechub.sharedkernel.logging.AuditLogService;
@@ -19,46 +20,47 @@ public class DeleteProductExecutionProfileServiceTest {
     private DeleteProductExecutionProfileService serviceToTest;
     private ProductExecutionProfileRepository repository;
 
-    @Before
-    public void before() throws Exception {
+    @BeforeEach
+    void beforeEach() throws Exception {
+        repository = mock(ProductExecutionProfileRepository.class);
         serviceToTest = new DeleteProductExecutionProfileService();
 
-        serviceToTest.profileIdValidation = mock(ProductExecutionProfileIdValidation.class);
-        ValidationResult validResult = new ValidationResult();
-        when(serviceToTest.profileIdValidation.validate(any())).thenReturn(validResult);
-        repository = mock(ProductExecutionProfileRepository.class);
         serviceToTest.auditLogService = mock(AuditLogService.class);
         serviceToTest.repository = repository;
+        serviceToTest.profileIdValidation = mock(ProductExecutionProfileIdValidation.class);
+
+        when(serviceToTest.profileIdValidation.validate(any())).thenReturn(new ValidationResult());
     }
 
     @Test
-    public void existing_profile_can_be_deleted() {
+    void existing_profile_can_be_deleted() {
         /* prepare */
-        ProductExecutionProfile profile = new ProductExecutionProfile();
-        profile.id = "profileid1";
-        profile.description = "d1";
-        profile.projectIds.add("project1");
-        profile.enabled = false;
-        Optional<ProductExecutionProfile> opt = Optional.of(profile);
-        when(repository.findById("profileid1")).thenReturn(opt);
+        String profileId = "profileid1";
+        
+        when(repository.findById(profileId)).thenReturn(Optional.of(mock(ProductExecutionProfile.class)));
 
         /* execute */
-        serviceToTest.deleteProductExecutionProfile(profile.id);
+        serviceToTest.deleteProductExecutionProfile(profileId);
 
         /* test */
-        verify(repository).deleteById(profile.id);
+        verify(repository).deleteById(profileId); // check delete method was called
     }
     
-    @Test(expected = NotFoundException.class)
-    public void non_existing_profile_cannot_be_deleted_and_throws_not_found_exception() {
+    @Test
+    void non_existing_profile_cannot_be_deleted_and_throws_not_found_exception() {
         /* prepare */
-        when(repository.findById("profileid1")).thenReturn(Optional.empty());
+        String profileId = "not-existing";
+        
+        when(repository.findById(any())).thenReturn(Optional.empty()); 
 
-        /* execute */
-        serviceToTest.deleteProductExecutionProfile("not-existing");
+        /* execute + test exception*/
+        assertThrows(NotFoundException.class, ()->{
+            serviceToTest.deleteProductExecutionProfile(profileId);
+        });
 
         /* test*/
-        // test done by epxected part
+        verify(repository).findById(profileId); // check findById was used
+        verify(repository,never()).deleteById(any()); // check delete method was never called
     }
 
 }
