6a899e4d28df3829f23898d25b7ac7cdc1fe4be7
Albert Tregnaghi <albert.tregnaghi@daimler.com>
2022-02-16 07:47:45 +0100

    Review changes #651
    - temp folder for integration tests now also inside build folder
    - changed log statement so AbstractSharedVolumePropertiesSetup.java
      doing a fallback because missing configured upload directory
      will be logged correctly. Also changed name of folder so it
      is clear it's a fallback
    - Changed TextFileWriterTest so it uses now TestUtil creation
       methods for "subFolder" as well. Also changed to junit5

--- sechub-doc/src/test/java/com/daimler/sechub/docgen/util/TextFileWriterTest.java
+++ sechub-doc/src/test/java/com/daimler/sechub/docgen/util/TextFileWriterTest.java
@@ -7,33 +7,39 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.daimler.sechub.test.TestUtil;
 
 public class TextFileWriterTest {
 	
 	private TextFileWriter writerToTest;
 
 
-	@Before
-	public void before() throws Exception {
+	@BeforeEach
+	void before() throws Exception {
 		writerToTest = new TextFileWriter();
 	}
 
 
 	@Test
-	public void is_able_to_save_a_temporary_file() throws Exception {
+	void is_able_to_save_a_file_having_path_not_createad_before() throws Exception {
 		/* prepare */
-		File file = new File(System.getProperty("java.io.tmpdir"));
-		File subFolder = new File(file, "subFolder");
+		File file = TestUtil.createTempDirectoryInBuildFolder("textfilewriter-test").toFile();
+		File subFolder = new File(file, "subFolder"); 
 		File targetFile = new File(subFolder, "targetFile");
 		targetFile.deleteOnExit();
+
+		assertFalse(subFolder.exists());
+		assertFalse(targetFile.exists());
 
 		/* execute */
 		writerToTest.save(targetFile, "text");
 
 		/* test */
-		assertTrue(targetFile.exists());
+		assertTrue(subFolder.exists());
+		assertTrue(targetFile.exists()); 
 
 		try (BufferedReader br = new BufferedReader(new FileReader(targetFile))) {
 			String line = br.readLine();
