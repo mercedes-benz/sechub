48284e36f41b8dc686e607e0083de1b7fb0c28d8
jeeppler <jeremias.eppler@daimler.com>
2022-02-18 23:40:25 +0100

    flawfinder adjust output #1018

--- sechub-pds-solutions/multi/docker/scripts/flawfinder.sh
+++ sechub-pds-solutions/multi/docker/scripts/flawfinder.sh
@@ -3,3 +3,8 @@
 
 cd "$PDS_JOB_SOURCECODE_UNZIPPED_FOLDER/"
 flawfinder --sarif . > "$PDS_JOB_RESULT_FILE"
+
+# remove "./ in front of uri locations
+# workaround for https://github.com/david-a-wheeler/flawfinder/issues/67
+# use | character as separator
+sed --in-place 's|"uri": "./|"uri": "|g' "$PDS_JOB_RESULT_FILE"
