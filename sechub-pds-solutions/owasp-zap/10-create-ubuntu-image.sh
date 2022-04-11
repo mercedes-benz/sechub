172b3b2c60da0910108921c199071da0b0aabb1e
Jeeppler <Jeeppler@users.noreply.github.com>
2022-04-11 14:14:13 +0200

    Feature 607 provide pds zap integration (#876)
    * First version #607
    
    * Install firefox, jre and owasp-zap inside container #607
    
    - added RUN command to install owasp zap in current version 2.10
    - added TODO inside run.sh
    
    * Added first approach of scanning with the owasp zap
    
    * make docker-compose files work
    
    * added mocks
    
    * upgraded owasp zap and pds
    
    * Helm chart, container and docu #607
    
    * Several improvements
    
    - new object and shared volume.svg
    - improved plugin caching by using wget
    - added single worker thread option
    - changed API key name
    - upgraded alpine images to 3.15
    - replaced MinIO with SeaweedFS
    
    * Remove AGPL notice and mention SeaweedFS
    
    * changed api key name in helm, added zap report 0.1.0
    
    * upgrade ZAP and plugins, new ZAP version
    
    * upgraded PDS and Helm chart
    
    * fix permission issues
    
    * Added support for additional proxy #607
    
    * Introduced target type and upgraded zap addons #607
    
    * build owasp zap wrapper #607
    
    * changes from review and upgrade to PDS 0.27.0 #607
    
    * format docker compose yaml and add probes #607
    
    Co-authored-by: Jan Winz <jan.winz@daimler.com>

---
+++ sechub-pds-solutions/owasp-zap/10-create-ubuntu-image.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+
+REGISTRY="$1"
+VERSION="$2"
+BASE_IMAGE="$3"
+
+if [[ -z "$REGISTRY" ]]
+then
+  echo "Please provide a docker registry server."
+  exit 1
+fi
+
+if [[ -z "$VERSION" ]]
+then
+  echo "Please provide a version for the container."
+  exit 1
+fi
+
+if [[ -z "$BASE_IMAGE" ]]
+then
+    BASE_IMAGE="ubuntu:20.04"
+fi
+
+echo ">> Base image: $BASE_IMAGE"
+docker build --pull --no-cache --build-arg BASE_IMAGE=$BASE_IMAGE --tag "$REGISTRY:$VERSION" --file docker/Owasp-Zap-Ubuntu.dockerfile docker/
+docker tag "$REGISTRY:$VERSION" "$REGISTRY:latest"
