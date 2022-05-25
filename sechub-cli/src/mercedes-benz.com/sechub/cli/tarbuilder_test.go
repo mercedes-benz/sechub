4cc2772da560a7cf4b949edf6e582a8d39922b0a
Sven Dolderer <sven.dolderer@mercedes-benz.com>
2022-05-25 12:52:55 +0200

    unit tests added for client binary support #1155
    also fixed AssertSize's output

---
+++ sechub-cli/src/mercedes-benz.com/sechub/cli/tarbuilder_test.go
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: MIT
+
+package cli
+
+import (
+	"os"
+	"testing"
+
+	sechubTestUtil "mercedes-benz.com/sechub/testutil"
+	sechubUtil "mercedes-benz.com/sechub/util"
+)
+
+func Test_createBinariesTarFile_EmptyTarFileIsRejected(t *testing.T) {
+	/* prepare */
+	var context Context
+	var config Config
+	var sechubConfig SecHubConfig
+
+	context.config = &config
+	context.sechubConfig = &sechubConfig
+
+	dir := sechubTestUtil.InitializeTestTempDir(t)
+	defer os.RemoveAll(dir)
+
+	dirname1 := dir + "/sub1"
+	dirname2 := dir + "/sub2"
+	dirname3 := dir + "/sub2/sub3_with_a_veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeery_loooooooooooooong_name"
+
+	sechubTestUtil.CreateTestDirectory(dirname1, 0755, t)
+	sechubTestUtil.CreateTestDirectory(dirname2, 0755, t)
+	sechubTestUtil.CreateTestDirectory(dirname3, 0755, t)
+
+	// We add empty folders only, so the tar archive will be empty (containing only directories).
+	// This should trigger an error, because uploading empty archives makes no sense.
+
+	context.binariesTarFileName = dir + "/testoutput.tar"
+	scanConfig := NamedBinariesScanConfig{
+		Name:       "my-test",
+		FileSystem: FileSystemConfig{Folders: []string{dirname1, dirname2}},
+	}
+	context.sechubConfig.Data.Binaries = []NamedBinariesScanConfig{scanConfig}
+
+	/* execute */
+	err := createBinariesTarFile(&context)
+
+	/* test */
+	sechubTestUtil.AssertErrorHasExpectedMessage(err, sechubUtil.TarFileHasNoContent, t)
+}
+
+func Test_createBinariesTarFile_DataBinariesSectionWorksWithAbsolutePathes(t *testing.T) {
+	/* prepare */
+	var context Context
+	var config Config
+	var sechubConfig SecHubConfig
+	context.config = &config
+	context.sechubConfig = &sechubConfig
+
+	// dir is abolute path
+	dir := sechubTestUtil.InitializeTestTempDir(t)
+	defer os.RemoveAll(dir)
+
+	// create subdirectories
+	dirname1 := dir + "/sub1"
+	dirname2 := dir + "/sub2"
+	dirname3 := dir + "/sub2/sub3"
+	sechubTestUtil.CreateTestDirectory(dirname1, 0755, t)
+	sechubTestUtil.CreateTestDirectory(dirname2, 0755, t)
+	sechubTestUtil.CreateTestDirectory(dirname3, 0755, t)
+
+	// create files
+	filepath1 := dirname1 + "/file-sub1.bin"
+	filepath2 := dirname2 + "/file-sub2.bin"
+	filepath3 := dirname3 + "/file-sub3.bin"
+	filepath4 := dir + "/standalone1.bin"
+	filepath5 := dir + "/standalone2.bin"
+	content := []byte("Binary :-)\n")
+	sechubTestUtil.CreateTestFile(filepath1, 0644, content, t)
+	sechubTestUtil.CreateTestFile(filepath2, 0644, content, t)
+	sechubTestUtil.CreateTestFile(filepath3, 0644, content, t)
+	sechubTestUtil.CreateTestFile(filepath4, 0644, content, t)
+	sechubTestUtil.CreateTestFile(filepath5, 0644, content, t)
+
+	// create configs
+	namedBinariesScanConfig1 := NamedBinariesScanConfig{
+		Name:       "testname1",
+		FileSystem: FileSystemConfig{Files: []string{filepath4, filepath5}, Folders: []string{dirname1}},
+	}
+
+	namedBinariesScanConfig2 := NamedBinariesScanConfig{
+		Name:       "testname2",
+		FileSystem: FileSystemConfig{Folders: []string{dirname2}},
+	}
+
+	context.sechubConfig.Data.Binaries = []NamedBinariesScanConfig{namedBinariesScanConfig1, namedBinariesScanConfig2}
+	context.binariesTarFileName = dir + "/test.tar"
+
+	/* execute */
+	createBinariesTarFile(&context)
+
+	/* test */
+	list, _ := sechubUtil.ListContentOfTarFile(context.binariesTarFileName)
+	sechubTestUtil.AssertContains(list, archiveDataPrefix+"/"+namedBinariesScanConfig1.Name+filepath1, t)
+	sechubTestUtil.AssertContains(list, archiveDataPrefix+"/"+namedBinariesScanConfig1.Name+filepath4, t)
+	sechubTestUtil.AssertContains(list, archiveDataPrefix+"/"+namedBinariesScanConfig1.Name+filepath5, t)
+	sechubTestUtil.AssertContains(list, archiveDataPrefix+"/"+namedBinariesScanConfig2.Name+filepath2, t)
+	sechubTestUtil.AssertContains(list, archiveDataPrefix+"/"+namedBinariesScanConfig2.Name+filepath3, t)
+	sechubTestUtil.AssertSize(list, 5, t)
+}
+
+func Test_createBinariesTarFile_DataBinariesSectionWorksWithRelativePathes(t *testing.T) {
+	/* prepare */
+	var context Context
+	var config Config
+	var sechubConfig SecHubConfig
+	context.config = &config
+	context.sechubConfig = &sechubConfig
+
+	// dir is relative path
+	dir := "sechub-cli-tmptest"
+	sechubTestUtil.CreateTestDirectory(dir, 0755, t)
+	defer os.RemoveAll(dir)
+
+	// create subdirectories
+	dirname1 := dir + "/sub1"
+	sechubTestUtil.CreateTestDirectory(dirname1, 0755, t)
+
+	// create files
+	filepath1 := dirname1 + "/file-sub1.bin"
+	filepath2 := dir + "/standalone1.bin"
+	content := []byte("Binary :-)\n")
+	sechubTestUtil.CreateTestFile(filepath1, 0644, content, t)
+	sechubTestUtil.CreateTestFile(filepath2, 0644, content, t)
+
+	// create configs
+	namedBinariesScanConfig1 := NamedBinariesScanConfig{
+		Name:       "testname1",
+		FileSystem: FileSystemConfig{Files: []string{filepath2}, Folders: []string{dirname1}},
+	}
+
+	context.sechubConfig.Data.Binaries = []NamedBinariesScanConfig{namedBinariesScanConfig1}
+	context.binariesTarFileName = dir + "/test.tar"
+
+	/* execute */
+	createBinariesTarFile(&context)
+
+	/* test */
+	list, _ := sechubUtil.ListContentOfTarFile(context.binariesTarFileName)
+	sechubTestUtil.AssertContains(list, archiveDataPrefix+"/"+namedBinariesScanConfig1.Name+"/"+filepath1, t)
+	sechubTestUtil.AssertContains(list, archiveDataPrefix+"/"+namedBinariesScanConfig1.Name+"/"+filepath2, t)
+	sechubTestUtil.AssertSize(list, 2, t)
+}
---
+++ sechub-cli/src/mercedes-benz.com/sechub/util/tar_test.go
@@ -0,0 +1,289 @@
+// SPDX-License-Identifier: MIT
+package util
+
+import (
+	"archive/tar"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	sechubUtil "mercedes-benz.com/sechub/testutil"
+)
+
+func TestTarFileTarsItselfIsRejected(t *testing.T) {
+	/* prepare */
+	dir := sechubUtil.InitializeTestTempDir(t)
+	defer os.RemoveAll(dir)
+
+	dirname1 := dir + "/sub1"
+	sechubUtil.CreateTestDirectory(dirname1, 0755, t)
+
+	filename1 := dirname1 + "/file1.bin"
+
+	content := []byte("Binary :-)\n")
+	sechubUtil.CreateTestFile(filename1, 0644, content, t)
+
+	// path to tar file is also part of added files - because in dirname1
+	tarFile := dirname1 + "/testoutput.tar"
+	// create tar file
+	newTarFile, _ := os.Create(tarFile)
+	// create tar writer
+	tarWriter := tar.NewWriter(newTarFile)
+
+	config := TarConfig{
+		TarFileName: tarFile,
+		TarWriter:   tarWriter,
+		PrefixInTar: "",
+		Folders:     []string{dirname1},
+	}
+
+	/* execute */
+	err := Tar(&config)
+	tarWriter.Close()
+	newTarFile.Close()
+
+	/* test */
+	sechubUtil.AssertErrorHasExpectedMessage(err, TargetTarFileLoop, t)
+}
+
+func TestTarFileCanBeCreated(t *testing.T) {
+	/* prepare */
+	dir := sechubUtil.InitializeTestTempDir(t)
+	defer os.RemoveAll(dir)
+
+	dirname1 := dir + "/sub1"
+	dirname2 := dir + "/sub2"
+	dirname3 := dir + "/sub2/sub3"
+
+	sechubUtil.CreateTestDirectory(dirname1, 0755, t)
+	sechubUtil.CreateTestDirectory(dirname2, 0755, t)
+	sechubUtil.CreateTestDirectory(dirname3, 0755, t)
+
+	filename1 := dirname1 + "/file1.bin"
+	filename2 := dirname2 + "/file2.bin"
+	filename3 := dirname3 + "/file3.bin"
+
+	content := []byte("Binary :-)\n")
+	sechubUtil.CreateTestFile(filename1, 0644, content, t)
+	sechubUtil.CreateTestFile(filename2, 0644, content, t)
+	sechubUtil.CreateTestFile(filename3, 0644, content, t)
+
+	path := dir + "/testoutput.tar"
+	newTarFile, _ := os.Create(path)
+	tarWriter := tar.NewWriter(newTarFile)
+
+	config := TarConfig{
+		TarFileName: path,
+		TarWriter:   tarWriter,
+		PrefixInTar: "__data__/test/",
+		Files:       []string{filename1},
+		Folders:     []string{dirname2},
+	}
+
+	/* execute */
+	err := Tar(&config)
+	tarWriter.Close()
+	newTarFile.Close()
+
+	/* test */
+	sechubUtil.Check(err, t)
+	sechubUtil.AssertFileExists(path, t)
+
+	list, _ := ListContentOfTarFile(path)
+	sechubUtil.AssertContains(list, config.PrefixInTar+strings.TrimPrefix(filename1, "/"), t)
+	sechubUtil.AssertContains(list, config.PrefixInTar+strings.TrimPrefix(filename2, "/"), t)
+	sechubUtil.AssertContains(list, config.PrefixInTar+strings.TrimPrefix(filename3, "/"), t)
+	sechubUtil.AssertSize(list, 3, t)
+}
+
+func TestTarFileCanBeCreated_with_exclude_patterns_applied(t *testing.T) {
+	/* prepare */
+	dir := sechubUtil.InitializeTestTempDir(t)
+	defer os.RemoveAll(dir)
+
+	dirname1 := dir + "/sub1"
+	dirname2 := dir + "/sub2"
+	dirname3 := dir + "/sub2/sub3"
+
+	sechubUtil.CreateTestDirectory(dirname1, 0755, t)
+	sechubUtil.CreateTestDirectory(dirname2, 0755, t)
+	sechubUtil.CreateTestDirectory(dirname3, 0755, t)
+
+	filename0 := dirname1 + "/file0.txt"
+	filename1 := dirname1 + "/file1.bin"
+	filename2 := dirname2 + "/file2.bin"
+	filename3 := dirname3 + "/file3.bin"
+
+	content := []byte("Binary :-)\n")
+	sechubUtil.CreateTestFile(filename0, 0644, content, t)
+	sechubUtil.CreateTestFile(filename1, 0644, content, t)
+	sechubUtil.CreateTestFile(filename2, 0644, content, t)
+	sechubUtil.CreateTestFile(filename3, 0644, content, t)
+
+	path := dir + "/testoutput.tar"
+	newTarFile, _ := os.Create(path)
+	tarWriter := tar.NewWriter(newTarFile)
+
+	config := TarConfig{
+		TarFileName: path,
+		TarWriter:   tarWriter,
+		PrefixInTar: "",
+		Folders:     []string{dirname1, dirname2},
+		Excludes:    []string{"**/sub3/**", "*.txt"},
+	}
+
+	/* execute */
+	err := Tar(&config)
+	tarWriter.Close()
+	newTarFile.Close()
+
+	/* test */
+	sechubUtil.Check(err, t)
+
+	list, _ := ListContentOfTarFile(path)
+	sechubUtil.AssertContainsNot(list, strings.TrimPrefix(filename0, "/"), t) // pattern excluded
+	sechubUtil.AssertContains(list, strings.TrimPrefix(filename1, "/"), t)    // this must remain
+	sechubUtil.AssertContains(list, strings.TrimPrefix(filename2, "/"), t)    // this must remain
+	sechubUtil.AssertContainsNot(list, strings.TrimPrefix(filename3, "/"), t) // folder excluded
+	sechubUtil.AssertSize(list, 2, t)
+}
+
+func TestTarFileNonExistingFolderIsRejected(t *testing.T) {
+	/* prepare */
+	dir := sechubUtil.InitializeTestTempDir(t)
+	defer os.RemoveAll(dir)
+
+	dirname1 := dir + "/nonexistant"
+
+	path := dir + "/testoutput.tar"
+	newTarFile, _ := os.Create(path)
+	tarWriter := tar.NewWriter(newTarFile)
+
+	config := TarConfig{
+		TarFileName: path,
+		TarWriter:   tarWriter,
+		PrefixInTar: "",
+		Folders:     []string{dirname1},
+	}
+
+	/* execute */
+	err := Tar(&config)
+	tarWriter.Close()
+	newTarFile.Close()
+
+	/* test */
+	sechubUtil.AssertErrorHasExpectedStartMessage(err, "Folder not found:", t)
+}
+
+func TestTarFileContainsRelativeDefinedFolders(t *testing.T) {
+	/* prepare */
+	RelativeTmpTestDir := "sechub-cli-tmptest"
+	sechubUtil.CreateTestDirectory(RelativeTmpTestDir, 0755, t)
+	defer os.RemoveAll(RelativeTmpTestDir)
+
+	dirname1 := RelativeTmpTestDir + "/sub1"
+	dirname2 := RelativeTmpTestDir + "/sub2"
+	filepath1 := dirname1 + "/file1.bin"
+	filepath2 := dirname2 + "/file2.bin"
+
+	// create dirs
+	sechubUtil.CreateTestDirectory(dirname1, 0755, t)
+	sechubUtil.CreateTestDirectory(dirname2, 0755, t)
+
+	// create files
+	content := []byte("Binary :-)\n")
+	sechubUtil.CreateTestFile(filepath1, 0644, content, t)
+	sechubUtil.CreateTestFile(filepath2, 0644, content, t)
+
+	path := RelativeTmpTestDir + "/testoutput.tar"
+	newTarFile, _ := os.Create(path)
+	tarWriter := tar.NewWriter(newTarFile)
+
+	config := TarConfig{
+		TarFileName: path,
+		TarWriter:   tarWriter,
+		PrefixInTar: "",
+		Files:       []string{filepath1},
+		Folders:     []string{dirname2},
+	}
+
+	/* execute */
+	err := Tar(&config)
+	tarWriter.Close()
+	newTarFile.Close()
+
+	/* test */
+	sechubUtil.Check(err, t)
+
+	list, _ := ListContentOfTarFile(path)
+	sechubUtil.AssertContains(list, filepath1, t)
+	sechubUtil.AssertContains(list, filepath2, t)
+}
+
+func TestTarFileContainsRelativeFoldersOutsideCurrent(t *testing.T) {
+	/* prepare */
+	RelativeTmpTestDir := "./../sechub-cli-tmptest"
+	sechubUtil.CreateTestDirectory(RelativeTmpTestDir, 0755, t)
+	defer os.RemoveAll(RelativeTmpTestDir)
+
+	filepath1 := RelativeTmpTestDir + "/file1.bin"
+
+	// create files
+	content := []byte("Binary :-)\n")
+	sechubUtil.CreateTestFile(filepath1, 0644, content, t)
+
+	path := RelativeTmpTestDir + "/testoutput.tar"
+	newTarFile, _ := os.Create(path)
+	tarWriter := tar.NewWriter(newTarFile)
+
+	config := TarConfig{
+		TarFileName: path,
+		TarWriter:   tarWriter,
+		PrefixInTar: "",
+		Folders:     []string{RelativeTmpTestDir},
+		Excludes:    []string{"*.tar"}, // exclude our own tar
+	}
+
+	/* execute */
+	err := Tar(&config)
+	tarWriter.Close()
+	newTarFile.Close()
+
+	/* test */
+	sechubUtil.Check(err, t)
+
+	list, _ := ListContentOfTarFile(path)
+	// "./../sechub-cli-tmptest/file1.bin" becomes "sechub-cli-tmptest/file1.bin" in tar file
+	sechubUtil.AssertContains(list, "sechub-cli-tmptest/file1.bin", t)
+}
+
+func Example_tarDetectsNonExistingFiles() {
+	/* prepare */
+	var t testing.T
+	RelativeTmpTestDir := "sechub-cli-tmptest"
+	sechubUtil.CreateTestDirectory(RelativeTmpTestDir, 0755, &t)
+	defer os.RemoveAll(RelativeTmpTestDir)
+
+	path := RelativeTmpTestDir + "/testoutput.tar"
+	newTarFile, _ := os.Create(path)
+	tarWriter := tar.NewWriter(newTarFile)
+
+	config := TarConfig{
+		TarFileName: path,
+		TarWriter:   tarWriter,
+		PrefixInTar: "",
+		Files:       []string{RelativeTmpTestDir + "/non-existing-file.bin"},
+	}
+
+	/* execute */
+	err := Tar(&config)
+	tarWriter.Close()
+	newTarFile.Close()
+
+	/* test */
+	fmt.Println(err)
+	// Output:
+	// Folder created: "sechub-cli-tmptest"
+	// open sechub-cli-tmptest/non-existing-file.bin: no such file or directory
+}
