3cfcc3722d8880c82d207eb6c32f6f13af5bec82
jeeppler <jeremias.eppler@mercedes-benz.com>
2022-06-10 23:43:27 +0200

    additional information in output #1373 #1383
    - addining information to the output
    - improved formatting of output
    - fix mock source

--- sechub-pds-solutions/scancode/docker/scripts/scancode.sh
+++ sechub-pds-solutions/scancode/docker/scripts/scancode.sh
@@ -13,50 +13,25 @@ scancode_processes="1"
 # parameters and fail
 additional_options=""
 
-echo "-------------"
-echo "SecHub Config"
-echo "-------------"
+echo ""
+echo "------"
+echo "System"
+echo "------"
+echo ""
+
+printf "%-26s %s\n" "PDS version:" "$PDS_VERSION"
+printf "%-26s %s\n" "Scancode-Toolkit version:" "$SCANCODE_VERSION"
+
+echo ""
+echo "---------"
+echo "PDS Setup"
+echo "---------"
+echo ""
 
 echo "SecHub Job UUID: $SECHUB_JOB_UUID"
-
-echo "-------"
-echo " Setup "
-echo "-------"
-
-if [[ "$SCANCODE_PROCESSES" -ge 1 ]]
-then
-    scancode_processes="$SCANCODE_PROCESSES"
-    additional_options="$additional_options --processes $scancode_processes"
-fi
-
-if [[ -n "$SCANCODE_LICENSE_SCORE" ]]
-then
-    if [[ "$SCANCODE_LICENSE_SCORE" -le 100 && "$SCANCODE_LICENSE_SCORE" -ge 0 ]]
-    then
-        echo "between 0 and 100"
-        license_score="$SCANCODE_LICENSE_SCORE"
-        additional_options="$additional_options --license-score $license_score"
-    fi
-fi
-
-given_output_format="$(echo "$SCANCODE_OUTPUT_FORMAT" | tr '[:upper:]' '[:lower:]')"
-
-echo "Given format: $given_output_format"
-
-# is given output format in output formats?
-if [[ $output_formats =~ " $given_output_format " ]]
-then
-    echo "Given format matches."
-
-    if [[ "$given_output_format" == "spdx-json" ]]
-    then
-        echo "Format: --spdx-json"
-    else
-        convert_output_to_spdx_json=false
-        output_format="--$given_output_format"
-        echo "Format: $output_format"
-    fi 
-fi
+# TODO: Add PDS Job UUID - requires PDS containing #863
+# The workspace location contains the PDS Job UUID
+echo "PDS Job Workspace Location: $PDS_JOB_WORKSPACE_LOCATION"
 
 extracted_folder=""
 if $PDS_JOB_HAS_EXTRACTED_SOURCES
@@ -74,6 +49,46 @@ echo ""
 tree "$extracted_folder"
 
 echo ""
+echo "--------------"
+echo "Scancode Setup"
+echo "--------------"
+echo ""
+
+echo "User provided number of scancode processes: $SCANCODE_PROCESSES"
+if [[ "$SCANCODE_PROCESSES" -ge 1 ]]
+then
+    scancode_processes="$SCANCODE_PROCESSES"
+    additional_options="$additional_options --processes $scancode_processes"
+fi
+
+echo "User provided license score: $SCANCODE_LICENSE_SCORE"
+if [[ -n "$SCANCODE_LICENSE_SCORE" ]]
+then
+    if [[ "$SCANCODE_LICENSE_SCORE" -le 100 && "$SCANCODE_LICENSE_SCORE" -ge 0 ]]
+    then
+        license_score="$SCANCODE_LICENSE_SCORE"
+        additional_options="$additional_options --license-score $license_score"
+    fi
+fi
+
+given_output_format="$(echo "$SCANCODE_OUTPUT_FORMAT" | tr '[:upper:]' '[:lower:]')"
+
+echo "Possible output formats: $output_formats"
+echo "User provided output format: $given_output_format"
+
+# is given output format in output formats?
+if [[ $output_formats =~ " $given_output_format " ]]
+then
+    echo "User provided output format is in possible output formats."
+
+    if [[ "$given_output_format" != "spdx-json" ]]
+    then
+        convert_output_to_spdx_json=false
+        output_format="--$given_output_format"
+    fi 
+fi
+
+echo ""
 echo "-------------"
 echo "Starting scan"
 echo "-------------"
@@ -83,6 +98,7 @@ echo "Scancode processes: $scancode_processes"
 echo "Minimum license score: $license_score"
 echo "Additional options: $additional_options"
 echo "Output format: $output_format"
+echo ""
 
 spdx_file="$PDS_JOB_RESULT_FILE.spdx"
 
@@ -93,6 +109,7 @@ then
   "$TOOL_FOLDER/scancode-toolkit-$SCANCODE_VERSION/extractcode" $extracted_folder
 fi
 
+# `2>&1` -> redirect the verbose output from standard error to standard out
 "$TOOL_FOLDER/scancode-toolkit-$SCANCODE_VERSION/scancode" $additional_options --verbose --strip-root --copyright --license --package --email --url --info $output_format $spdx_file $extracted_folder 2>&1
 
 echo ""
