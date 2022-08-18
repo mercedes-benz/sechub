5d066bce00d64e6dc26b1f6409d504907c416aee
Sven Dolderer <sven.dolderer@mercedes-benz.com>
2022-08-18 16:43:19 +0200

    x-file-size header set #1552
    - also HTTP debugging consolidated

--- sechub-cli/src/mercedes-benz.com/sechub/cli/controller-uploadsourcezip.go
+++ sechub-cli/src/mercedes-benz.com/sechub/cli/controller-uploadsourcezip.go
@@ -3,7 +3,6 @@
 package cli
 
 import (
-	"fmt"
 	"os"
 
 	sechubUtil "mercedes-benz.com/sechub/util"
@@ -24,14 +23,7 @@ func uploadSourceZipFile(context *Context) {
 	request, err := newFileUploadRequestViaPipe(buildUploadSourceCodeAPICall(context), extraParams, "file", context.sourceZipFileName)
 	sechubUtil.HandleError(err, ExitCodeIOError)
 
-	if context.config.debug || context.config.debugHTTP {
-		sechubUtil.LogDebug(true, fmt.Sprintf("HTTP %v %v", request.Method, request.URL))
-		sechubUtil.LogDebug(true, fmt.Sprintf("Content length of HTTP upload request: %d bytes", request.ContentLength))
-	}
-
-	request.SetBasicAuth(context.config.user, context.config.apiToken)
-
-	handleHTTPRequestAndResponse(context, request) // HTTP call for upload
+	handleHTTPRequestAndResponse(context, request, false) // HTTP call for upload
 }
 
 func uploadBinariesTarFile(context *Context) {
@@ -49,12 +41,5 @@ func uploadBinariesTarFile(context *Context) {
 	request, err := newFileUploadRequestViaPipe(buildUploadBinariesAPICall(context), extraParams, "file", context.binariesTarFileName)
 	sechubUtil.HandleError(err, ExitCodeIOError)
 
-	if context.config.debug || context.config.debugHTTP {
-		sechubUtil.LogDebug(true, fmt.Sprintf("HTTP %v %v", request.Method, request.URL))
-		sechubUtil.LogDebug(true, fmt.Sprintf("Content length of HTTP upload request: %d bytes", request.ContentLength))
-	}
-
-	request.SetBasicAuth(context.config.user, context.config.apiToken)
-
-	handleHTTPRequestAndResponse(context, request) // HTTP call for upload
+	handleHTTPRequestAndResponse(context, request, false) // HTTP call for upload
 }
