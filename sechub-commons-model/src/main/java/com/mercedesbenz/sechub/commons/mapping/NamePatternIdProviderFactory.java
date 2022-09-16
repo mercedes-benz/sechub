8e5afbf243ac34956f059447daa3114413209290
Albert Tregnaghi <albert.tregnaghi@mercedes-benz.com>
2022-09-16 12:55:10 +0200

    Name pattern ID provider can now handle empty or null json data #1655
    - this is a global way to handle such problems. The presetID - which
      is optional - can now be empty or null without any problems

--- sechub-commons-model/src/main/java/com/mercedesbenz/sechub/commons/mapping/NamePatternIdProviderFactory.java
+++ sechub-commons-model/src/main/java/com/mercedesbenz/sechub/commons/mapping/NamePatternIdProviderFactory.java
@@ -3,7 +3,14 @@ package com.mercedesbenz.sechub.commons.mapping;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class NamePatternIdProviderFactory {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NamePatternIdProviderFactory.class);
+
+    private static final String EMPTY_JSON = "{}";
 
     MappingDataToNamePatternToIdEntryConverter converter;
 
@@ -13,6 +20,14 @@ public class NamePatternIdProviderFactory {
 
     public NamePatternIdProvider createProvider(String id, String mappingDataAsJson) {
         NamePatternIdProvider provider;
+
+        if (mappingDataAsJson == null || mappingDataAsJson.trim().isEmpty()) {
+
+            LOG.info("Given data for provider with id: {} was: '{}'. To have at least valid Json we use instead: '{}'.", mappingDataAsJson, EMPTY_JSON);
+            mappingDataAsJson = EMPTY_JSON;
+
+        }
+
         MappingData mappingData = MappingData.fromString(mappingDataAsJson);
         List<NamePatternToIdEntry> entries = converter.convert(mappingData);
 
