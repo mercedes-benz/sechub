0ebb65b77f1d4a1b8d42b95534400ef6d55516ff
Albert Tregnaghi <albert.tregnaghi@mercedes-benz.com>
2022-09-16 16:51:12 +0200

    Added automated debug logging when PDS_DEBUG_ENABLED is true #1655

--- sechub-pds-solutions/checkmarx/docker/checkmarx.sh
+++ sechub-pds-solutions/checkmarx/docker/checkmarx.sh
@@ -6,6 +6,8 @@ echo "# Starting Checkmarx PDS-solution                    #"
 echo "######################################################"
 echo "- SECHUB_JOB_UUID : $SECHUB_JOB_UUID"
 
+options="";
+
 if [[ "$PDS_DEBUG_ENABLED" = "true" ]]; then
     echo ""
     echo "  PDS DEBUG:"
@@ -16,9 +18,10 @@ if [[ "$PDS_DEBUG_ENABLED" = "true" ]]; then
     echo "  - PDS_SCAN_CONFIGURATION                 : $PDS_SCAN_CONFIGURATION"
     echo "  - CHECKMARX_NEWPROJECT_TEAMID_MAPPING    : $CHECKMARX_NEWPROJECT_TEAMID_MAPPING"
     echo "  - CHECKMARX_NEWPROJECT_PRESETID_MAPPING  : $CHECKMARX_NEWPROJECT_PRESETID_MAPPING"
+    
+    options="$options -Dlogging.level.com.mercedesbenz.sechub=DEBUG"
 fi
 
-options="";
 
 options="$options -Dspring.profiles.active=default"
 
