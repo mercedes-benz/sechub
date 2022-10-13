631d6dd46331e7b14e40a62dd43344c4ecf1c021
Valentyn Grygoriev <zigfridus@gmail.com>
2022-10-13 14:29:39 +0300

    Some fixes #673:
    - use Junit5 instead of Junit4;
    - add check for null argument;
    - some code readibility improvements.

--- sechub-pds/src/main/java/com/mercedesbenz/sechub/pds/security/PDSPasswordTransformer.java
+++ sechub-pds/src/main/java/com/mercedesbenz/sechub/pds/security/PDSPasswordTransformer.java
@@ -8,8 +8,13 @@ package com.mercedesbenz.sechub.pds.security;
 public class PDSPasswordTransformer {
 
     public String transformPassword(String originPassword) {
+        if (originPassword == null) {
+            throw new IllegalArgumentException("Password may not be null!");
+        }
+
         if (originPassword.startsWith("{noop}") || originPassword.startsWith("{bcrypt}"))
             return originPassword;
+
         return "{noop}" + originPassword;
     }
 }
--- sechub-pds/src/test/java/com/mercedesbenz/sechub/pds/security/PDSPasswordTransformerTest.java
+++ sechub-pds/src/test/java/com/mercedesbenz/sechub/pds/security/PDSPasswordTransformerTest.java
@@ -1,39 +1,61 @@
 // SPDX-License-Identifier: MIT
 package com.mercedesbenz.sechub.pds.security;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class PDSPasswordTransformerTest {
+class PDSPasswordTransformerTest {
 
-    private PDSPasswordTransformer passwordTransformer;
+    private PDSPasswordTransformer transformerToTest;
 
-    @Before
-    public void before() {
-        passwordTransformer = new PDSPasswordTransformer();
+    @BeforeEach
+    void beforeEach() {
+        transformerToTest = new PDSPasswordTransformer();
     }
 
     @Test
-    public void transform_password_without_noop() {
+    void transform_password_without_noop() {
+        /* prepare */
         String originPassword = "my-pwd";
-        String transformedPassword = passwordTransformer.transformPassword(originPassword);
+
+        /* execute */
+        String transformedPassword = transformerToTest.transformPassword(originPassword);
+
+        /* test */
         assertEquals("{noop}my-pwd", transformedPassword);
     }
 
     @Test
-    public void skip_password_with_noop() {
+    void skip_password_with_noop() {
+        /* prepare */
         String originPassword = "{noop}my-pwd";
-        String transformedPassword = passwordTransformer.transformPassword(originPassword);
+
+        /* execute */
+        String transformedPassword = transformerToTest.transformPassword(originPassword);
+
+        /* test */
         assertEquals("{noop}my-pwd", transformedPassword);
     }
 
     @Test
-    public void skip_password_with_bcrypt() {
+    void skip_password_with_bcrypt() {
+        /* prepare */
         String originPassword = "{bcrypt}my-pwd";
-        String transformedPassword = passwordTransformer.transformPassword(originPassword);
+
+        /* execute */
+        String transformedPassword = transformerToTest.transformPassword(originPassword);
+
+        /* test */
         assertEquals("{bcrypt}my-pwd", transformedPassword);
     }
 
+    @Test
+    void null_argument() {
+        /* execute + test */
+        assertThrows(IllegalArgumentException.class, () -> {
+            transformerToTest.transformPassword(null);
+        });
+    }
 }
