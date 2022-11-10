2106b7aef9362fadd421d37629b971e8f6b3d890
Jan Winz <jan.winz@mercedes-benz.com>
2022-11-10 14:22:15 +0100

    Review Changes #1730
    - added comment to -silent parameter of ZAPROXY
    - update of network extension, due to bug fix

--- sechub-pds-solutions/owasp-zap/docker/run.sh
+++ sechub-pds-solutions/owasp-zap/docker/run.sh
@@ -50,6 +50,10 @@ localserver () {
         echo " * Accesskey: $S3_ACCESSKEY"
     fi
 
+	# -silent: disables telemetry calls, of the call home addon: https://www.zaproxy.org/docs/desktop/addons/call-home/
+	#   This addon is mandatory now but the telemetry calls can be deactivated.
+	#   This feature addtionally disables automated update calls, e.g. to update extensions.
+	#   Otherwise, if you want to use a specific versions of extensions e.g. for testing reasons, ZAP would automatically check for updates.
     nohup owasp-zap -daemon -silent -nostdout -host 127.0.0.1 -port 8080 -config api.key="$ZAP_API_KEY" &
 
     # Regarding entropy collection:
