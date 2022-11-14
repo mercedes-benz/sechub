2b4f2903dbd09dce64616ccc5b3dbcb5c149042e
Sven Dolderer <sven.dolderer@mercedes-benz.com>
2022-11-14 17:04:24 +0100

    cleanup pds run scripts #1721
    - call /run_additional.sh directly
    - group storage options together
    - removed functions hiding what is really called
    - remove obsolete run.sh

--- sechub-pds-solutions/pds-base/docker/run_additional.sh
+++ sechub-pds-solutions/pds-base/docker/run_additional.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 # SPDX-License-Identifier: MIT
 
-run_additional() {
-    # this function is called from the main run.sh script
-    # overwrite this file in a container based on pds-base
-    # if additional functionality is needed
-    echo ""
-}
+# This script is called from the main run.sh script.
+# Overwrite this file in a container based on pds-base
+# if additional functionality is needed.
+# E.g. to start background processes etc.
+
+echo "No additional actions."
