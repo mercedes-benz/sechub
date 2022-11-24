db93229ea93c2be9973bd594fdd6b36561ae0e44
Jan Winz <jan.winz@mercedes-benz.com>
2022-11-24 14:15:33 +0100

    Implement start PDS for every scan and shutdown afterwards #1757
    - update PDS-OWASPZAP to use base image on docker compose scripts
    - extend owasp-zap.sh to start and stop OWASP-ZAP for every scan
    - remove run_additional.sh for this PDS-Solution

--- sechub-pds-solutions/owasp-zap/docker/scripts/owasp-zap.sh
+++ sechub-pds-solutions/owasp-zap/docker/scripts/owasp-zap.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env sh
 # SPDX-License-Identifier: MIT
 
-sechub_job_uuid=$(echo "$PDS_JOB_WORKSPACE_LOCATION" | cut -d "/" -f 4)
+shutdownZAP() {
+	pkill -9 -f "/usr/bin/owasp-zap"
+	pkill -9 -f "/usr/share/zaproxy/zap-"
+}
 
-echo "Clean up possible old session data before starting sechub job: $sechub_job_uuid"
-rm -r ~/.ZAP/session/*
-rm ~/.ZAP/zap.log*
+ZAP_HOST="127.0.0.1"
+ZAP_PORT="8080"
+
+echo "Starting up OWASP-ZAP server."
+# -silent: disables telemetry calls, of the call home addon: https://www.zaproxy.org/docs/desktop/addons/call-home/
+#   This addon is mandatory now but the telemetry calls can be deactivated.
+#   This feature addtionally disables automated update calls, e.g. to update extensions.
+#   Otherwise, if you want to use a specific versions of extensions e.g. for testing reasons, ZAP would automatically check for updates.
+owasp-zap -daemon -silent -nostdout -host $ZAP_HOST -port $ZAP_PORT -config api.key=$ZAP_API_KEY &
+
+echo "Waiting for OWASP-ZAP to start..."
+
+wget -qO - --retry-connrefused --tries=20 --waitretry=5 --header="Accept: application/json" --header="X-ZAP-API-Key: $ZAP_API_KEY" http://$ZAP_HOST:$ZAP_PORT/JSON/core/view/version
+if [ $? -ne 0 ]
+then
+    echo "OWASP-ZAP did not start after waiting for 2 minutes."
+    shutdownZAP
+    exit 1;
+fi
+echo "OWASP-ZAP started."
+
+sechub_job_uuid=$(echo "$PDS_JOB_WORKSPACE_LOCATION" | cut -d "/" -f 4)
 
 echo "sechub job uuid: $sechub_job_uuid"
 echo ""
@@ -66,3 +88,12 @@ then
 fi
 
 java -jar $TOOL_FOLDER/wrapperowaspzap.jar $options --zapHost 127.0.0.1 --jobUUID "$sechub_job_uuid" --zapPort 8080 --targetURL "$PDS_SCAN_TARGET_URL" --report "$PDS_JOB_RESULT_FILE" --fullRulesetfile $TOOL_FOLDER/owasp-zap-full-ruleset-all-release-status.json
+
+
+# Shutdown and cleanup after scan
+echo "Shutdown OWASP-ZAP after scan."
+shutdownZAP
+
+echo "Clean up possible old session data after scan."
+rm -r ~/.ZAP/session/*
+rm ~/.ZAP/zap.log*
