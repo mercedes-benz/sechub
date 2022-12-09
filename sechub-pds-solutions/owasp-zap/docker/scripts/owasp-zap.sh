3e18dba0074419e625b0b9d820d70f218c9becf0
Jan Winz <jan.winz@mercedes-benz.com>
2022-12-09 09:14:25 +0100

    PR Review Changes #1784
    - added comments to wget call
    - improved comments
    - use already available SECHUB_JOB_UUID
    - use variables to specify wget retry information

--- sechub-pds-solutions/owasp-zap/docker/scripts/owasp-zap.sh
+++ sechub-pds-solutions/owasp-zap/docker/scripts/owasp-zap.sh
@@ -2,9 +2,9 @@
 # SPDX-License-Identifier: MIT
 
 shutdownZAP() {
-	# -f to specify the process by looking at the parameters
-	pkill -9 -f "/usr/bin/owasp-zap"
-	pkill -9 -f "/usr/share/zaproxy/zap-"
+	# --full: to specify the process by looking at full command line including the parameters
+	pkill -9 --full "/usr/bin/owasp-zap"
+	pkill -9 --full "/usr/share/zaproxy/zap-"
 }
 
 # Start OWASP-ZAP server
@@ -13,22 +13,32 @@ echo "Starting up OWASP-ZAP server"
 #   This addon is mandatory now but the telemetry calls can be deactivated.
 #   This feature addtionally disables automated update calls, e.g. to update extensions.
 #   Otherwise, if you want to use a specific versions of extensions e.g. for testing reasons, ZAP would automatically check for updates.
-owasp-zap -daemon -silent -nostdout -host $ZAP_HOST -port $ZAP_PORT -config api.key=$ZAP_API_KEY &
+owasp-zap -daemon -silent -nostdout -host "$ZAP_HOST" -port "$ZAP_PORT" -config "api.key=$ZAP_API_KEY" &
 
 echo "Waiting for OWASP-ZAP to start"
-wget --quiet --output-document=- --retry-connrefused --tries=20 --waitretry=6 --header="Accept: application/json" --header="X-ZAP-API-Key: $ZAP_API_KEY" --no-check-certificate https://$ZAP_HOST:$ZAP_PORT/JSON/core/view/version
+TRIES=20
+WAITRETRY=6
+TOTAL_WAITTIME=$( echo "$TRIES*$WAITRETRY" | bc )
+
+# To check if OWASP-ZAP is up and running we try to call api for the OWASP-ZAP version
+# --quiet: suppress the default output of wget
+# --output-document: specify where to save the output to, "--output-document=-" specifies STDOUT
+# --retry-connrefused: retry if connection was refused
+# --tries: how often the connection shall be retried
+# --waitretry: maximum amount of seconds before retrying to connect
+# --header: headers needed for the request
+# --no-check-certificate: OWASP-ZAP uses a self-signed certificate, because of this we skip the certificate validation
+wget --quiet --output-document=- --retry-connrefused --tries="$TRIES" --waitretry="$WAITRETRY" --header="Accept: application/json" --header="X-ZAP-API-Key: $ZAP_API_KEY" --no-check-certificate "https://$ZAP_HOST:$ZAP_PORT/JSON/core/view/version"
+
 if [ $? -ne 0 ]
 then
-    echo "OWASP-ZAP did not start after waiting for 2 minutes"
+    echo "OWASP-ZAP did not start after waiting for $TOTAL_WAITTIME minutes"
     shutdownZAP
     exit 1
 fi
 echo "OWASP-ZAP started"
-
 # Execute scan
-sechub_job_uuid=$(echo "$PDS_JOB_WORKSPACE_LOCATION" | cut -d "/" -f 4)
-
-echo "sechub job uuid: $sechub_job_uuid"
+echo "sechub job uuid: $SECHUB_JOB_UUID"
 echo ""
 
 echo "###########################"
@@ -86,8 +96,7 @@ then
     options="$options --sechubConfigfile $sechub_scan_configuration"
 fi
 
-java -jar $TOOL_FOLDER/wrapperowaspzap.jar $options --zapHost $ZAP_HOST --zapPort $ZAP_PORT --zapApiKey $ZAP_API_KEY --jobUUID "$sechub_job_uuid" --targetURL "$PDS_SCAN_TARGET_URL" --report "$PDS_JOB_RESULT_FILE" --fullRulesetfile $TOOL_FOLDER/owasp-zap-full-ruleset-all-release-status.json
-
+java -jar "$TOOL_FOLDER/wrapperowaspzap.jar" $options --zapHost "$ZAP_HOST" --zapPort "$ZAP_PORT" --zapApiKey "$ZAP_API_KEY" --jobUUID "$SECHUB_JOB_UUID" --targetURL "$PDS_SCAN_TARGET_URL" --report "$PDS_JOB_RESULT_FILE" --fullRulesetfile "$TOOL_FOLDER/owasp-zap-full-ruleset-all-release-status.json"
 
 # Shutdown OWASP-ZAP and cleanup after the scan
 echo "Shutdown OWASP-ZAP after scan"
