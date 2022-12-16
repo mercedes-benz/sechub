6bcebd9a840ccc4a93dfb07be24a3338bd9cc37b
jeeppler <jeremias.eppler@mercedes-benz.com>
2022-12-16 21:41:12 +0100

    Activate SSL by default

---
+++ sechub-webui/dev-create_localhost_certificate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT
+
+source dev-base.sh
+
+if [ -f "$DEV_CERT_FILE" ]; then
+    echo "Remove old localhost certificate"
+    rm $DEV_CERT_FILE
+fi
+createLocalhostCertifcate
