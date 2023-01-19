a49edf2de21af0002559bbb4ff997a8c4ffc9c9b
jeeppler <jeremias.eppler@mercedes-benz.com>
2023-01-19 18:22:50 +0100

    Fix spotless #1147

--- sechub-webui/src/main/java/com/mercedesbenz/sechub/webui/configuration/WebClientFactory.java
+++ sechub-webui/src/main/java/com/mercedesbenz/sechub/webui/configuration/WebClientFactory.java
@@ -9,13 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import reactor.netty.http.client.HttpClient;
-
 import com.mercedesbenz.sechub.webui.SecHubServerAccessService;
 
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientFactory {
