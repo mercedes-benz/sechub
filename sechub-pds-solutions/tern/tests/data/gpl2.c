d6a702adcfa84adbde8a5f6613f6aa5bbbb67505
Jeeppler <Jeeppler@users.noreply.github.com>
2023-01-23 16:58:02 +0100

    Feature 1147 webui skeleton (#1614)
    * initial version #1147
    
    * Basic WebUI skeleton #1147
    
    - implement basic WebUI skeleton #1147
      - user can request new API Token
      - user can login/loggout
      - user can request the server version
      - multi-language support added
    
    * Fix formatting
    
    * Added some tests #1147
    
    * Changes based on review #1147
    
    - simple CSS layout
    - restructured HTML
    - added banner. Message: webui not production ready
    - removed unnecessary configuration files and parameters
    - sechub url and other parameters now in SecHubServerAccess
    - introduced UserDetailInformationService
    
    * Apply spotless, fix gradle #1147
    
    * Rewrite to be reactive #1147
    
    * Rewrote tests for JUnit5 and reactive
    
    * Activate SSL by default
    
    * Headers apply #1147
    
    * Introduce credential service and test profile, fix test #1147
    
    * Seal api token #1147
    
    * Use the RANDOM_PORT strategy for testing
    
    * Document testing behaviour, introduce RANDOM_PORT strategy #1147
    
    * Fixes from review #1147
    
    - remove SPDX identifier from Tern test files. Created issue #1877 to find a long term solution. Goes beyond the scope of WebUI issue.
    - use multiple files for profiles #1147
    
    * Fix spotless #1147
    
    * Fixes from review #1147
    
    - language resources are now under `i18n`
    - fixed class and value names

--- sechub-pds-solutions/tern/tests/data/gpl2.c
+++ sechub-pds-solutions/tern/tests/data/gpl2.c
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// SPDX-License-Identifier: GPL-2.0-only
 
 # include <stdio.h>
 
--- sechub-pds-solutions/tern/tests/data/gpl3.py
+++ sechub-pds-solutions/tern/tests/data/gpl3.py
@@ -1,4 +1,3 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-License-Identifier: MIT
 
 print("This file is licensed under GPL 3+")
