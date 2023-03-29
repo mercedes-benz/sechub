ac657b411ef084b448aa44770990b7ed88543901
Sven Dolderer <sven.dolderer@mercedes-benz.com>
2023-03-29 16:37:33 +0200

    excludes work when creating .tar files #2127

--- sechub-cli/src/mercedes-benz.com/sechub/util/tar_test.go
+++ sechub-cli/src/mercedes-benz.com/sechub/util/tar_test.go
@@ -288,3 +288,107 @@ func Example_tarDetectsNonExistingFiles() {
 	// Folder created: "sechub-cli-tmptest"
 	// open sechub-cli-tmptest/non-existing-file.bin: no such file or directory
 }
+
+func TestTarFileCanBeCreated_with_absolute_path_and_exclude_patterns_applied(t *testing.T) {
+	/* prepare */
+	dir := sechubUtil.InitializeTestTempDir(t)
+	defer os.RemoveAll(dir)
+
+	dirname1 := dir + "/sub1"
+	dirname2 := dir + "/sub2"
+	dirname3 := dir + "/sub2/sub3"
+
+	sechubUtil.CreateTestDirectory(dirname1, 0755, t)
+	sechubUtil.CreateTestDirectory(dirname2, 0755, t)
+	sechubUtil.CreateTestDirectory(dirname3, 0755, t)
+
+	filename0 := dirname1 + "/file0.txt"
+	filename1 := dirname1 + "/file1.txt"
+	filename2 := dirname2 + "/file2.txt"
+	filename3 := dirname3 + "/file3.txt"
+
+	content := []byte("Hello world!\n")
+	sechubUtil.CreateTestFile(filename0, 0644, content, t)
+	sechubUtil.CreateTestFile(filename1, 0644, content, t)
+	sechubUtil.CreateTestFile(filename2, 0644, content, t)
+	sechubUtil.CreateTestFile(filename3, 0644, content, t)
+
+	path := dir + "/testoutput.tar"
+	newTarFile, _ := os.Create(path)
+	tarWriter := tar.NewWriter(newTarFile)
+
+	config := TarConfig{
+		TarFileName: path,
+		TarWriter:   tarWriter,
+		PrefixInTar: "test123/",
+		Folders:     []string{dir},
+		Excludes:    []string{dirname2 + "/**", "*.tar"},
+	}
+
+	/* execute */
+	err := Tar(&config)
+	tarWriter.Close()
+	newTarFile.Close()
+
+	/* test */
+	sechubUtil.Check(err, t)
+	sechubUtil.AssertFileExists(path, t)
+
+	list, _ := ListContentOfTarFile(path)
+	sechubUtil.AssertContains(list, strings.TrimPrefix("test123"+filename0, "/"), t)    // this must remain
+	sechubUtil.AssertContains(list, strings.TrimPrefix("test123"+filename1, "/"), t)    // this must remain
+	sechubUtil.AssertContainsNot(list, strings.TrimPrefix("test123"+filename2, "/"), t) // excluded!
+	sechubUtil.AssertContainsNot(list, strings.TrimPrefix("test123"+filename3, "/"), t) // excluded!
+	sechubUtil.AssertSize(list, 2, t)
+}
+
+func TestTarFileCanBeCreated_with_relative_path_and_exclude_patterns_applied(t *testing.T) {
+	/* prepare */
+	RelativeTmpTestDir := "sechub-cli-tmptest"
+	sechubUtil.CreateTestDirectory(RelativeTmpTestDir, 0755, t)
+	defer os.RemoveAll(RelativeTmpTestDir)
+
+	dirname1 := RelativeTmpTestDir + "/sub1"
+	dirname2 := RelativeTmpTestDir + "/sub2"
+	dirname3 := RelativeTmpTestDir + "/sub2/sub3"
+	filepath1 := dirname1 + "/file1.txt"
+	filepath2 := dirname2 + "/file1.txt"
+	filepath3 := dirname3 + "/file1.txt"
+
+	// create dirs
+	sechubUtil.CreateTestDirectory(dirname1, 0755, t)
+	sechubUtil.CreateTestDirectory(dirname2, 0755, t)
+	sechubUtil.CreateTestDirectory(dirname3, 0755, t)
+
+	// create files
+	content := []byte("Hello world!\n")
+	sechubUtil.CreateTestFile(filepath1, 0644, content, t)
+	sechubUtil.CreateTestFile(filepath2, 0644, content, t)
+	sechubUtil.CreateTestFile(filepath3, 0644, content, t)
+
+	path := RelativeTmpTestDir + "/testoutput.tar"
+	newTarFile, _ := os.Create(path)
+	tarWriter := tar.NewWriter(newTarFile)
+
+	config := TarConfig{
+		TarFileName: path,
+		TarWriter:   tarWriter,
+		PrefixInTar: "test123/",
+		Folders:     []string{RelativeTmpTestDir},
+		Excludes:    []string{dirname2 + "/**", "*.tar"},
+	}
+
+	/* execute */
+	err := Tar(&config)
+	tarWriter.Close()
+	newTarFile.Close()
+
+	/* test */
+	sechubUtil.Check(err, t)
+	sechubUtil.AssertFileExists(path, t)
+
+	list, _ := ListContentOfTarFile(path)
+	sechubUtil.AssertContains(list, "test123/"+filepath1, t)
+	sechubUtil.AssertContainsNot(list, "test123/"+filepath2, t) // excluded!
+	sechubUtil.AssertContainsNot(list, "test123/"+filepath3, t) // excluded!
+}
