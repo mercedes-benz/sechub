2a8ecde1f2da8cb0a56844e5bca7eb8e83d1a427
Albert Tregnaghi <albert.tregnaghi@mercedes-benz.com>
2023-04-18 16:21:11 +0200

    SecHubConfigurationModelValidationError has now ending punctuation #2186
    - wrote unit test
    - added missing punctuation

---
+++ sechub-commons-model/src/test/java/com/mercedesbenz/sechub/commons/model/SecHubConfigurationModelValidationErrorTest.java
@@ -0,0 +1,32 @@
+package com.mercedesbenz.sechub.commons.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+class SecHubConfigurationModelValidationErrorTest {
+
+    @ParameterizedTest
+    @EnumSource(SecHubConfigurationModelValidationError.class)
+    void message_does_end_with_dot_or_exclamation_mark(SecHubConfigurationModelValidationError error) {
+        /* execute */
+        String defaultMessage = error.getDefaultMessage();
+
+        /* test */
+        String[] expectedEndings = new String[] { ".", "!" };
+        boolean endsWithExpected = false;
+        for (String expectedEnding : expectedEndings) {
+            endsWithExpected = defaultMessage.endsWith(expectedEnding);
+            if (endsWithExpected) {
+                break;
+            }
+
+        }
+
+        if (!endsWithExpected) {
+            fail("Message does not end with expected endings:" + expectedEndings + ":\n" + defaultMessage);
+        }
+    }
+
+}
