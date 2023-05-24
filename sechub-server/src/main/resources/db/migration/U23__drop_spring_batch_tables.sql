06cfc7bd70c580445ead81fd08586e97c86a7ab2
Albert Tregnaghi <albert.tregnaghi@mercedes-benz.com>
2023-05-24 17:34:15 +0200

    Changed event trace tests #723
    - event trace test failed, because class changed from
      anonymous to "normal"
    - fixed typo

--- sechub-server/src/main/resources/db/migration/U23__drop_spring_batch_tables.sql
+++ sechub-server/src/main/resources/db/migration/U23__drop_spring_batch_tables.sql
@@ -1,5 +1,5 @@
 -- SPDX-License-Identifier: MIT
 
--- We do nothing here. Reason: If a migration to a lower version is necessaray, this means also
+-- We do nothing here. Reason: If a migration to a lower version is necessary, this means also
 -- that we have an older server implementation up and running.
 -- In this case the (old) server version will automatically create the missing spring batch tables
