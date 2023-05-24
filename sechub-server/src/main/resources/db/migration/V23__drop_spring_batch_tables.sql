f983d09d5b13f07d6c904837d42072a373da90b0
Albert Tregnaghi <albert.tregnaghi@mercedes-benz.com>
2023-05-24 16:59:22 +0200

    Drop spring batch tables by flyway #723

---
+++ sechub-server/src/main/resources/db/migration/V23__drop_spring_batch_tables.sql
@@ -0,0 +1,8 @@
+-- SPDX-License-Identifier: MIT
+DROP TABLE IF EXISTS batch_step_execution_context;
+DROP TABLE IF EXISTS batch_step_execution;
+
+DROP TABLE IF EXISTS batch_job_execution_params;
+DROP TABLE IF EXISTS batch_job_execution_context;
+DROP TABLE IF EXISTS batch_job_execution;
+DROP TABLE IF EXISTS batch_job_instance;
