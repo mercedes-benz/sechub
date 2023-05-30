c3df24a92ac51272d19124a3d80f18dccece22ab
Albert Tregnaghi <albert.tregnaghi@mercedes-benz.com>
2023-05-30 16:32:37 +0200

    Introduce continuous deployment for PDS #2273
    - Introduce hard fallback to READY_TO_START when a
      SIGTERM signal is detected.
    - introduce force query to update PDS status state
      very fast for multiple job uuids
    - Added some tests, changed some Junit4 tests to Junit5
    - Added log info with details about forced restarts for jobs

--- sechub-pds/src/main/java/com/mercedesbenz/sechub/pds/execution/PDSExecutionService.java
+++ sechub-pds/src/main/java/com/mercedesbenz/sechub/pds/execution/PDSExecutionService.java
@@ -7,10 +7,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -106,7 +108,23 @@ public class PDSExecutionService {
          * and not a spring component. Because of this, we listen here to the spring
          * boot destroy signal and shutdown the executor service by our own:
          */
-        scheduler.shutdown();
+        scheduler.shutdown(); // will stop processing new parts!
+
+        LOG.info("Scheduler executor service shutdown done");
+
+        Set<UUID> jobsToRestart = new LinkedHashSet<>();
+        Iterator<Entry<UUID, Future<PDSExecutionResult>>> it = jobsInQueue.entrySet().iterator();
+        while (it.hasNext()) {
+            Entry<UUID, Future<PDSExecutionResult>> entry = it.next();
+            Future<PDSExecutionResult> future = entry.getValue();
+            if (!future.isDone()) {
+                /* still running - must be restarted by next instance */
+                UUID pdsJobUUID = entry.getKey();
+                jobsToRestart.add(pdsJobUUID);
+            }
+        }
+        LOG.info("Handling predestroy for {} jobs in queue.", jobsToRestart.size());
+        jobTransactionService.forceStateResetInOwnTransaction(jobsToRestart, PDSJobStatusState.READY_TO_START);
     }
 
     /**
