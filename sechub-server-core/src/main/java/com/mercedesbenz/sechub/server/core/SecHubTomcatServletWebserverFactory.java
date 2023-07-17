c8090d69a5c9d6ab9a0a9e0dae0c1dcb1b34f557
Albert Tregnaghi <albert.tregnaghi@mercedes-benz.com>
2023-07-17 15:40:12 +0200

    SecHubTomcatServletWebserverFactory can now handle MB,KB and B #2379
    - SecHubTomcatServletWebserverFactory does now calculate one additional
      MB also to entries which ends with "KB" or "B".
    - added test to check logic

--- sechub-server-core/src/main/java/com/mercedesbenz/sechub/server/core/SecHubTomcatServletWebserverFactory.java
+++ sechub-server-core/src/main/java/com/mercedesbenz/sechub/server/core/SecHubTomcatServletWebserverFactory.java
@@ -14,6 +14,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class SecHubTomcatServletWebserverFactory implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 
+    private static final int FACTOR_BYTES = 1;
+
+    private static final int FACTOR_KILOBYTESS = 1024;
+
+    private static final int FACTOR_MEGABYTES = 1024 * 1024;
+
     private static final Logger LOG = LoggerFactory.getLogger(SecHubTomcatServletWebserverFactory.class);
 
     @Value("${spring.servlet.multipart.max-file-size}")
@@ -28,16 +34,12 @@ public class SecHubTomcatServletWebserverFactory implements WebServerFactoryCust
            as defined in SecHubExceptionHandler
 
            @formatter:on*/
-        String size = maxFileSizeAsString.trim().toLowerCase();
-        if (!size.endsWith("mb")) {
-            LOG.error("Cannot customize tomcat swallow size automatically, because multipart size not supported:" + size);
+        int maxSwallowSize = calculateBytesForOneMegabyteMoreThan(maxFileSizeAsString);
+
+        if (maxSwallowSize == -1) {
+            LOG.error("Cannot customize tomcat swallow size automatically, because size format not supported:" + maxFileSizeAsString);
             return;
         }
-        size = size.substring(0, size.length() - 2);
-        size = size.trim();
-
-        int springUploadMaxFileSizeInMB = Integer.parseInt(size);
-        int maxSwallowSize = (springUploadMaxFileSizeInMB + 1) * 1024 * 1024;
 
         factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
             @Override
@@ -48,5 +50,44 @@ public class SecHubTomcatServletWebserverFactory implements WebServerFactoryCust
                 }
             }
         });
+    }
+
+    static int calculateBytesForOneMegabyteMoreThan(String givenSpringMaxUploadSize) {
+        if (givenSpringMaxUploadSize == null || givenSpringMaxUploadSize.isEmpty()) {
+            return -1;
+        }
+        String springUploadMaxFileSize = givenSpringMaxUploadSize.trim().toLowerCase();
+        if (springUploadMaxFileSize.isEmpty()) {
+            return -1;
+        }
+
+        int factor = 0;
+        int remove = 0;
+
+        if (springUploadMaxFileSize.endsWith("mb")) {
+            factor = FACTOR_MEGABYTES;
+            remove = 2;
+        } else if (springUploadMaxFileSize.endsWith("kb")) {
+            factor = FACTOR_KILOBYTESS;
+            remove = 2;
+        } else if (springUploadMaxFileSize.endsWith("b")) {
+            factor = FACTOR_BYTES;
+            remove = 1;
+        } else {
+            return -1;
+        }
+        if (springUploadMaxFileSize.length() < remove) {
+            return -1;
+        }
+        springUploadMaxFileSize = springUploadMaxFileSize.substring(0, springUploadMaxFileSize.length() - remove);
+        if (springUploadMaxFileSize.isEmpty()) {
+            return -1;
+        }
+        springUploadMaxFileSize = springUploadMaxFileSize.trim();
+
+        int springUploadMaxFileSizeAsInt = Integer.parseInt(springUploadMaxFileSize);
+        int maxSwallowSize = (springUploadMaxFileSizeAsInt) * factor + (1 * FACTOR_MEGABYTES);
+
+        return maxSwallowSize;
     }
 }
