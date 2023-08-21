13c45b78d00d78f15ba201c52fea043fb5716d98
Sven Dolderer <sven.dolderer@mercedes-benz.com>
2023-08-21 11:50:24 +0200

    file and dir name changes #2501

--- sechub-pds-solutions/loc/docker/scripts/cloc_mock.sh
+++ sechub-pds-solutions/loc/docker/scripts/cloc_mock.sh
@@ -3,4 +3,4 @@
 
 # Mock is scan output of: https://github.com/Contrast-Security-OSS/go-test-bench
 echo "Running PDS cloc Mock"
-cp "$MOCK_FOLDER/mock.json" "$PDS_JOB_RESULT_FILE"
+cp "$MOCK_FOLDER/cloc-mock.json" "$PDS_JOB_RESULT_FILE"
