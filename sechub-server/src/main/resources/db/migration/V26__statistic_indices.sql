97bedfe97631643a5dd1119ac1657cd854b656ad
Sven Dolderer <sven.dolderer@mercedes-benz.com>
2023-10-05 12:16:54 +0200

    removed obsolete index creation #2574

--- sechub-server/src/main/resources/db/migration/V26__statistic_indices.sql
+++ sechub-server/src/main/resources/db/migration/V26__statistic_indices.sql
@@ -2,7 +2,8 @@
 
 -- indices on statistics tables to enhance reporting performance
 
-CREATE INDEX IF NOT EXISTS i01_statistic_job_run_project ON statistic_job_run (project_id);
-CREATE INDEX IF NOT EXISTS i02_statistic_job_run_month ON statistic_job_run (DATE_TRUNC('month', created));
+CREATE INDEX IF NOT EXISTS i01_statistic_job_run_project
+    ON statistic_job_run (project_id);
 
-CREATE INDEX IF NOT EXISTS i01_statistic_job_run_data_filter ON statistic_job_run_data (type,id);
+CREATE INDEX IF NOT EXISTS i01_statistic_job_run_data_filter
+    ON statistic_job_run_data (type,id);
