8d49cc846c3f599c2a09e028047afa7583ef9be4
Valentyn Grygoriev <zigfridus@gmail.com>
2023-11-02 19:55:16 +0200

    Update sechub-commons-model/src/test/java/com/mercedesbenz/sechub/commons/model/SecHubReportMetaDataSummaryTest.java
    Co-authored-by: Jeeppler <Jeeppler@users.noreply.github.com>

--- sechub-commons-model/src/test/java/com/mercedesbenz/sechub/commons/model/SecHubReportMetaDataSummaryTest.java
+++ sechub-commons-model/src/test/java/com/mercedesbenz/sechub/commons/model/SecHubReportMetaDataSummaryTest.java
@@ -104,7 +104,7 @@ public class SecHubReportMetaDataSummaryTest {
     @Test
     void when_add_multiple_high_medium_low_findings_then_red_yellow_green_and_total_counters_must_be_increased() {
         /* execute */
-        for (int i = 0; i < 101; i++) {
+        for (int i = 1; i <= 4 ; i++) {
             secHubReportMetaDataSummary.reportScanHelper(highFinding);
             secHubReportMetaDataSummary.reportScanHelper(mediumFinding);
             secHubReportMetaDataSummary.reportScanHelper(lowFinding);
