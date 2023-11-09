96b95a0a41eab52b54cf86dcf0191d9b92ff19bd
lorriborri <laura.bottner@mercedes-benz.com>
2023-11-09 10:25:13 +0100

    Added mocks for test #2504

--- sechub-pds-solutions/xray/docker/scripts/xray_mock.sh
+++ sechub-pds-solutions/xray/docker/scripts/xray_mock.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 # SPDX-License-Identifier: MIT
 
+# returns scan result of Xray for digininja/dvwa version 9b787fc
+# mock can return SPDX or CycloneDX report
+
+mock_file="spdx_mock.json"
+# mock_file="cycloneDX_mock.json"
+
 echo "Running PDS Xray Mock"
-cp "$MOCK_FOLDER/mock.json" "$PDS_JOB_RESULT_FILE"
+cp "$MOCK_FOLDER/$mock_file" "$PDS_JOB_RESULT_FILE"
+
--- sechub-solution/setup-pds/setup-xray-mock.sh
+++ sechub-solution/setup-pds/setup-xray-mock.sh
@@ -11,7 +11,7 @@ check_sechub_server_setup "$0" "$SCRIPT_PARAMETERS"
 
 user="xrayuser-mock"
 project="test-xray-mock"
-executor_file_name="xray-mock"
+executor_file_name="xray_mock"
 profile="pds-xray-mock"
 
 setup_project_user_executor_profile "$project" "$user" "$executor_file_name" "$profile"
