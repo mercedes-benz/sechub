72b7df4e87193cfb4364663fb4081c0046e6d2c1
Jan Winz <jan.winz@mercedes-benz.com>
2023-11-13 17:37:26 +0100

    remove optional from client certificate password #2524

--- sechub-commons-model/src/test/java/com/mercedesbenz/sechub/commons/model/ClientCertificateConfigurationTest.java
+++ sechub-commons-model/src/test/java/com/mercedesbenz/sechub/commons/model/ClientCertificateConfigurationTest.java
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 package com.mercedesbenz.sechub.commons.model;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
