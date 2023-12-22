244392f433ae1250f6eeddf3b399634eedc5a31e
Sven Dolderer <sven.dolderer@mercedes-benz.com>
2023-12-22 12:48:10 +0100

    docker build extended #2762
    - builder stages introduced
    - options: copy + download
    - obsolete jdk install removed (java is already installed)
    - BUILDER_BASE_IMAGE made configurable

--- sechub-pds-solutions/xray/10-create-image.sh
+++ sechub-pds-solutions/xray/10-create-image.sh
@@ -7,6 +7,8 @@ REGISTRY="$1"
 VERSION="$2"
 BASE_IMAGE="$3"
 
+DEFAULT_BUILD_TYPE=download
+
 usage() {
   cat - <<EOF
 
@@ -16,7 +18,9 @@ for <docker registry> with tag <version tag>.
 Required: <base image> ; for example ghcr.io/mercedes-benz/sechub/pds-base
 
 Additionally these environment variables can be defined:
-- XRAY_WRAPPER_VERSION - xray version to use. E.g. 2.9.5
+- BUILD_TYPE - The build type of the Checkmarx-Wrapper. Defaults to "$DEFAULT_BUILD_TYPE"
+- BUILDER_BASE_IMAGE - Base image for the build containers (see dockerfile)
+- XRAY_WRAPPER_VERSION - xray wrapper version to use. E.g. 1.0.0
 EOF
 }
 
@@ -43,6 +47,17 @@ fi
 
 BUILD_ARGS="--build-arg BASE_IMAGE=$BASE_IMAGE"
 echo ">> Base image: $BASE_IMAGE"
+
+if [[ -z "$BUILD_TYPE" ]] ; then
+    BUILD_TYPE="$DEFAULT_BUILD_TYPE"
+fi
+BUILD_ARGS+=" --build-arg BUILD_TYPE=$BUILD_TYPE"
+echo ">> - Build type: $BUILD_TYPE"
+
+if [[ ! -z "$BUILDER_BASE_IMAGE" ]] ; then
+    BUILD_ARGS+=" --build-arg BUILDER_BASE_IMAGE=$BUILDER_BASE_IMAGE"
+    echo ">> - Builder base image: $BUILDER_BASE_IMAGE"
+fi
 
 if [[ ! -z "$XRAY_WRAPPER_VERSION" ]] ; then
     echo ">> Xray version: $XRAY_WRAPPER_VERSION"
