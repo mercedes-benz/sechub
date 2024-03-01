19077a7864dcf8c6f9ed5c63516e7b0ea7b8dfb5
Bernardo Oliveira <bernardo.oliveira@mercedes-benz.com>
2024-03-01 13:40:35 +0000

    feat: new sechub website

---
+++ sechub-website/app.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'SecHub | Your security guardian.',
+  description:
+    'SecHub is a free and open-source security platform that provides a central API to test software with different security tools.',
+  ogImage: '/og.jpg'
+});
+</script>
+
+<template>
+  <Header />
+  <main>
+    <HomeHero />
+    <HomeFeatures />
+    <HomeDownload />
+  </main>
+  <Footer />
+</template>
+
+<style lang="scss">
+html {
+  scroll-behavior: smooth;
+}
+
+.button {
+  @apply inline-flex w-full justify-center rounded-md bg-fern-600 px-4 py-2 text-sm font-medium text-white hover:bg-fern-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75;
+  &.is-outlined {
+    @apply border border-fern-600 bg-transparent text-fern-600 hover:bg-fern-50;
+  }
+}
+
+.container {
+  @apply mx-auto max-w-7xl px-4 sm:px-6 lg:px-8;
+}
+
+.menu-item {
+  @apply relative -mx-3 -my-2 rounded-lg px-3 py-2 text-sm text-gray-700 transition-colors delay-150 hover:text-fern-600 hover:delay-0;
+}
+</style>
---
+++ sechub-website/components/home/Download.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
+import { ChevronDownIcon } from '@heroicons/vue/20/solid';
+
+const links = [
+  {
+    name: 'Client',
+    href: 'https://mercedes-benz.github.io/sechub/latest/client-download.html'
+  },
+  {
+    name: 'Server',
+    href: 'https://mercedes-benz.github.io/sechub/latest/server-download.html'
+  },
+  {
+    name: 'Product Delegation Server',
+    href: 'https://mercedes-benz.github.io/sechub/latest/pds-download.html'
+  }
+];
+</script>
+
+<template>
+  <section aria-label="Download Sechub" id="download" class="relative overflow-hidden py-32">
+    <AbstractCircularAnimation />
+
+    <div class="container">
+      <div class="mx-auto max-w-md sm:text-center">
+        <h2 class="text-3xl font-medium tracking-tight text-gray-900 sm:text-4xl">Start Using Today!</h2>
+        <p class="mt-4 text-lg text-gray-700">
+          Download SecHub for seamless security integration. Orchestrates tools through a unified API. Scan
+          synchronously, break build pipelines. MIT License.
+        </p>
+
+        <Menu as="div" class="relative mt-8 inline-block text-left">
+          <div>
+            <MenuButton class="button">
+              Download
+              <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5 text-white hover:text-fern-100" aria-hidden="true" />
+            </MenuButton>
+          </div>
+
+          <transition
+            enter-active-class="transition duration-100 ease-out"
+            enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-in"
+            leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0"
+          >
+            <MenuItems
+              class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+            >
+              <MenuItem v-slot="{ active }" v-for="link in links" :key="link.name">
+                <NuxtLink
+                  :to="link.href"
+                  target="_blank"
+                  class="group flex w-full items-center rounded-md px-2 py-2 text-sm text-gray-700 transition-colors duration-300 hover:bg-fern-500 hover:text-white"
+                >
+                  {{ link.name }}
+                </NuxtLink>
+              </MenuItem>
+            </MenuItems>
+          </transition>
+        </Menu>
+      </div>
+    </div>
+  </section>
+</template>
