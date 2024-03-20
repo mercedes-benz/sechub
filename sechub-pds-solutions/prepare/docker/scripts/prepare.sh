c561ab1ed253226d124670e7dfe09eb27a0b732e
Laura <laura.bottner@mercedes-benz.com>
2024-03-20 16:47:34 +0100

    Added pds-solution for prepare wrapper #2984

---
+++ sechub-pds-solutions/prepare/docker/scripts/prepare.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# SPDX-License-Identifier: MIT
+
+echo ""
+echo "---------"
+echo "PDS Setup"
+echo "---------"
+echo ""
+echo "SecHub Job UUID: $SECHUB_JOB_UUID"
+echo "PDS Job UUID: $PDS_JOB_UUID"
+echo ""
+
+
+
