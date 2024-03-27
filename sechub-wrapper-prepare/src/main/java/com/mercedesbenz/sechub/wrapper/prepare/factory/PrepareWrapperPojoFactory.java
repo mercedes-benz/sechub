afd0ebcf40502d1f0bb7338deb45b7a30b259617
Laura <laura.bottner@mercedes-benz.com>
2024-03-27 14:32:01 +0100

    Removed circular dependencies #2986

--- sechub-wrapper-prepare/src/main/java/com/mercedesbenz/sechub/wrapper/prepare/factory/PrepareWrapperPojoFactory.java
+++ sechub-wrapper-prepare/src/main/java/com/mercedesbenz/sechub/wrapper/prepare/factory/PrepareWrapperPojoFactory.java
@@ -1,23 +1,14 @@
 // SPDX-License-Identifier: MIT
 package com.mercedesbenz.sechub.wrapper.prepare.factory;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import com.mercedesbenz.sechub.commons.TextFileWriter;
 import com.mercedesbenz.sechub.commons.model.RemoteCredentialContainerFactory;
-import com.mercedesbenz.sechub.commons.pds.PDSUserMessageSupport;
-import com.mercedesbenz.sechub.wrapper.prepare.cli.PrepareWrapperEnvironment;
 
 @Component
 public class PrepareWrapperPojoFactory {
-
-    @Autowired
-    PrepareWrapperEnvironment environment;
-
-    @Autowired
-    TextFileWriter writer;
 
     @Bean
     RemoteCredentialContainerFactory createRemoteCredentialFactory() {
@@ -29,9 +20,4 @@ public class PrepareWrapperPojoFactory {
         return new TextFileWriter();
     }
 
-    @Bean
-    PDSUserMessageSupport createUserMessageSupport() {
-        String userMessageFolder = environment.getPdsUserMessagesFolder();
-        return new PDSUserMessageSupport(userMessageFolder, writer);
-    }
 }
