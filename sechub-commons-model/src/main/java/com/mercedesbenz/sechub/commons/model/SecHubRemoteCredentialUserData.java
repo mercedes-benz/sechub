bd4add93bb65f3abedfedb140172adbeadba92f3
Laura <laura.bottner@mercedes-benz.com>
2024-04-11 17:41:59 +0200

    Added credentials to remote sechub configuration

---
+++ sechub-commons-model/src/main/java/com/mercedesbenz/sechub/commons/model/SecHubRemoteCredentialUserData.java
@@ -0,0 +1,30 @@
+package com.mercedesbenz.sechub.commons.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SecHubRemoteCredentialUserData {
+
+    public static final String PROPERTY_NAME = "name";
+    public static final String PROPERTY_PASSWORD = "password";
+
+    private String name;
+
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
