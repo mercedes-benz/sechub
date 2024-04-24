79c05e45f477df70da48c606ae8adab0262a9dc1
Jan Winz <jan.winz@mercedes-benz.com>
2024-04-24 10:47:07 +0200

    Make history scan for pds gitleaks configurable #3092

--- sechub-pds-solutions/gitleaks/docker/scripts/gitleaks.sh
+++ sechub-pds-solutions/gitleaks/docker/scripts/gitleaks.sh
@@ -14,22 +14,28 @@ scan_target_directory="$PDS_JOB_EXTRACTED_SOURCES_FOLDER"
 # The full path containing "$PDS_JOB_EXTRACTED_SOURCES_FOLDER" is not useful in the report.
 gitleaks_options="--log-level debug --config $TOOL_FOLDER/custom-gitleaks.toml --source . --report-format sarif --report-path $PDS_JOB_RESULT_FILE --exit-code 0"
 
-# If no '.git' directory was found we cannot scan the git history
-if [[ -z "$git_directory" ]]
+# If the history scan was disabled, a normal filesystem scan is performed.
+if [[ "$GITLEAKS_HISTORY_SCAN_ENABLED" = "false" ]]
 then
     gitleaks_options="$gitleaks_options --no-git"
-    echo "No .git folder was found. Perform secret scan on filesystem without history deepscan." | tee "$PDS_JOB_USER_MESSAGES_FOLDER"/nogit.txt
+    echo "History scan disabled. Perform secret scan on filesystem without history deepscan." | tee "$PDS_JOB_USER_MESSAGES_FOLDER"/history-scan-disabled.txt
+    
+# If no '.git' directory was found we cannot scan the git history
+elif [[ -z "$git_directory" ]]
+then
+    gitleaks_options="$gitleaks_options --no-git"
+    echo "No .git folder was found. Perform secret scan on filesystem without history deepscan." | tee "$PDS_JOB_USER_MESSAGES_FOLDER"/no-git.txt
 
 # If the value of 'git_directory' is not a valid directory there is more than a single result of the find command
 elif [[ ! -d "$git_directory" ]]
 then
     gitleaks_options="$gitleaks_options --no-git"
-    echo "Multiple .git folders were found. Perform secret scan on filesystem without history deepscan." | tee "$PDS_JOB_USER_MESSAGES_FOLDER"/multiplegit.txt
+    echo "Multiple .git folders were found. Perform secret scan on filesystem without history deepscan." | tee "$PDS_JOB_USER_MESSAGES_FOLDER"/multiple-git.txt
 
 # If exactly one '.git' directory was found we scan the git history
 else
     scan_target_directory="$repository_root_directory"
-    echo ".git folder was found. Perform secret scan with history deepscan." | tee "$PDS_JOB_USER_MESSAGES_FOLDER"/historyscan.txt
+    echo ".git folder was found. Perform secret scan with history deepscan." | tee "$PDS_JOB_USER_MESSAGES_FOLDER"/history-scan.txt
 fi
 
 echo "Running Gitleaks"
