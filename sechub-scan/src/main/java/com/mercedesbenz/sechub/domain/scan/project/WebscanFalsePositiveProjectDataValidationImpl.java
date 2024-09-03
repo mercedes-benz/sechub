836b1689facb1967c066c94ebd2a80f163277192
Jan Winz <jan.winz@mercedes-benz.com>
2024-09-03 09:19:58 +0200

    minor changes #3401

--- sechub-scan/src/main/java/com/mercedesbenz/sechub/domain/scan/project/WebscanFalsePositiveProjectDataValidationImpl.java
+++ sechub-scan/src/main/java/com/mercedesbenz/sechub/domain/scan/project/WebscanFalsePositiveProjectDataValidationImpl.java
@@ -13,6 +13,7 @@ import com.mercedesbenz.sechub.sharedkernel.validation.ValidationContext;
 public class WebscanFalsePositiveProjectDataValidationImpl extends AbstractValidation<WebscanFalsePositiveProjectData>
         implements WebscanFalsePositiveProjectDataValidation {
 
+    private static final String BACKSLASH = "\\";
     private static final String VALIDATOR_NAME = "webscan false positive project data validation";
     private static final String WILDCARD_AND_URL_SEPARATOR_REGEX = "^[\\.\\*/:]+$";
     private static final Pattern WILDCARD_AND_URL_SEPARATOR_PATTERN = Pattern.compile(WILDCARD_AND_URL_SEPARATOR_REGEX);
@@ -57,14 +58,14 @@ public class WebscanFalsePositiveProjectDataValidationImpl extends AbstractValid
     private void validateUrlPattern(ValidationContext<WebscanFalsePositiveProjectData> context, String urlPattern) {
         String name = "%s.%s".formatted(FalsePositiveProjectData.PROPERTY_WEBSCAN, WebscanFalsePositiveProjectData.PROPERTY_URLPATTERN);
         if (urlPattern == null || urlPattern.isBlank()) {
-            context.addError(getValidatorName(), ": The '%s' is mandatory and must not be empty!".formatted(name));
+            context.addError(getValidatorName(), ": The '%s' is mandatory and must be present!".formatted(name));
             return;
         }
-        if (urlPattern.contains("\\")) {
+        if (urlPattern.contains(BACKSLASH)) {
             context.addError(getValidatorName(), ": Inside '%s' no backslashes are allowed!".formatted(name));
         }
         if (WILDCARD_AND_URL_SEPARATOR_PATTERN.matcher(name).matches()) {
-            context.addError(getValidatorName(), ": The '%s': '%s' each element must consist of more than just wildcards!".formatted(name, urlPattern));
+            context.addError(getValidatorName(), ": The '%s': '%s' must consist of more than just wildcards!".formatted(name, urlPattern));
         }
     }
 
