7fb8f6d8ed231b58f4631e8e566fd531fadbc804
hamidonos <hamid.ahmetovic@mercedes-benz.com>
2024-09-12 17:11:10 +0200

    add setup-script.js wrapper around shell script to make it executable in action.yml

--- github-actions/scan/setup-script.js
+++ github-actions/scan/setup-script.js
@@ -1,5 +1,13 @@
 // SPDX-License-Identifier: MIT
 
+/*
+    Wrapper script to execute the setup-script.sh file.
+    This script is required because GitHub Actions runners for Node.js applications
+    do not support direct execution of bash scripts.
+
+    Refer to action.yml for additional details.
+*/
+
 const { exec } = require('child_process');
 
 exec('bash ./setup-script.sh', (error, stdout, stderr) => {
