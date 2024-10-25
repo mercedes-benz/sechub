147e3f86dfc2759b573c5d89dcd6f92343a34675
hamidonos <hamid.ahmetovic@mercedes-benz.com>
2024-10-25 12:33:44 +0200

    merge

--- sechub-web-server/src/main/java/com/mercedesbenz/sechub/webserver/RequestConstants.java
+++ sechub-web-server/src/main/java/com/mercedesbenz/sechub/webserver/RequestConstants.java
@@ -4,6 +4,7 @@ package com.mercedesbenz.sechub.webserver;
 public final class RequestConstants {
 
     public static final String ROOT = "/";
+
     public static final String LOGIN = "/login";
     public static final String HOME = "/home";
 
