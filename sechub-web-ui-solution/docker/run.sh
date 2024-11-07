7d0752d57bd8f2be6df5a8caa334d9a0993168f4
Sven Dolderer <sven.dolderer@mercedes-benz.com>
2024-11-07 18:21:20 +0100

    ssl keystore handling added #3596

--- sechub-web-ui-solution/docker/run.sh
+++ sechub-web-ui-solution/docker/run.sh
@@ -2,20 +2,28 @@
 # SPDX-License-Identifier: MIT
 
 debug () {
-    while true
-    do
-	    echo "Press [CTRL+C] to stop.."
-	    sleep 120
-    done
+  while true ; do
+    echo "Press [CTRL+C] to stop.."
+    sleep 120
+  done
 }
 
-if [ "$LOADBALANCER_START_MODE" != "server" ]
-then
-    debug
+install_ssl_certs () {
+  echo "### Installing SSL certs \"$WEB_UI_SSL_KEYSTOREALIAS\""
+}
+
+###############
+# main
+if [ "$WEB_UI_SSL_KEYSTORE_ALIAS" != "undefined" ] ; then
+  install_ssl_certs
 fi
 
-echo "Check configuration file"
+if [ "$LOADBALANCER_START_MODE" != "server" ] ; then
+  debug
+fi
+
+echo "### Checking configuration file"
 nginx -t
 
-echo "Start Nginx"
+echo "### Starting Nginx"
 nginx -g 'daemon off;'
