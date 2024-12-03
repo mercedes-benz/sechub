c3d7116243e6bf80a27d0a799cea4beb41b6745a
hamidonos <hamid.ahmetovic@mercedes-benz.com>
2024-12-03 20:32:27 +0100

    remove web-server gradle module

--- sechub-doc/src/test/java/com/mercedesbenz/sechub/restdoc/TestRestDocSecurityConfiguration.java
+++ sechub-doc/src/test/java/com/mercedesbenz/sechub/restdoc/TestRestDocSecurityConfiguration.java
@@ -1,15 +1,11 @@
 // SPDX-License-Identifier: MIT
 package com.mercedesbenz.sechub.restdoc;
 
-import com.mercedesbenz.sechub.spring.security.AES256Encryption;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.client.RestTemplate;
 
 import com.mercedesbenz.sechub.sharedkernel.security.SecHubSecurityConfiguration;
-
-import static org.mockito.Mockito.mock;
 
 @Import(SecHubSecurityConfiguration.class)
 class TestRestDocSecurityConfiguration {
