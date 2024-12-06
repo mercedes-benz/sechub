dc70253ec9457303f1343d3d5f2b416dad87c572
Sven Dolderer <sven.dolderer@mercedes-benz.com>
2024-12-06 18:26:09 +0100

    publishing script nuxt enabled #3002

--- sechub-doc/helperscripts/publish+git-add-releasedocs.sh
+++ sechub-doc/helperscripts/publish+git-add-releasedocs.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 
+GIT_RELEASE_BRANCH="master"
 DOCS_FILE_LIST=""
 DOCS_SOURCE_DIR="build/docs/final-html"
 DEST_DIR="../docs"
 DOCS_DEST_DIR="$DEST_DIR/latest"
 DOCS_IMAGE_DIR="images"
-GIT_RELEASE_BRANCH="master"
 WEBSITE_HOME="../sechub-website"
 WEBSITE_BUILD_DIR="$WEBSITE_HOME/.output/public"
 WEBSITE_FILE_LIST=""
 
 function website_add_changed_files {
   pushd "$WEBSITE_BUILD_DIR/" >/dev/null 2>&1
-  local website_files=`find . -type f | sed 'sX^./XX'`
+  local website_files=`/bin/ls | grep -v '_nuxt'`
   popd >/dev/null 2>&1
-
-  local nuxt_dir="$DEST_DIR/_nuxt"
-  if [ -d "$nuxt_dir" ] ; then
-    echo "# Wiping _nuxt destination directory"
-    # because file names include checksums and thus change each time
-    git rm -rf "$nuxt_dir/"
-  fi
 
   echo -n "# Adding changed or new website files:"
   for website_file in $website_files ; do
@@ -30,10 +23,38 @@ function website_add_changed_files {
       WEBSITE_FILE_LIST="$WEBSITE_FILE_LIST $website_file"
     fi
   done
+
+  for file in $WEBSITE_FILE_LIST ; do
+    /bin/cp "$WEBSITE_BUILD_DIR/$file" "$DEST_DIR/$file"
+    echo "git add \"$DEST_DIR/$file\""
+    git add "$DEST_DIR/$file"
+  done
   echo
 }
 
-function docs_add_changed_images {
+function website_add_nuxt_files {
+  local nuxt_dir="$DEST_DIR/_nuxt"
+
+  if [ -d "$nuxt_dir" ] ; then
+    echo "# Wiping _nuxt destination directory"
+    # because file names include checksums and thus change each time
+    git rm -rf "$nuxt_dir/"
+  fi
+
+  echo "# Copying _nuxt directory"
+  /bin/cp -r "$WEBSITE_BUILD_DIR/_nuxt" "$DEST_DIR"
+
+  pushd "$DEST_DIR/" >/dev/null 2>&1
+  local nuxt_files=`find _nuxt/ -type f`
+
+  for file in $nuxt_files ; do
+    echo "git add \"$file\""
+    git add "$file"
+  done
+  popd >/dev/null 2>&1
+}
+
+function docs_collect_changed_images {
   pushd "$DOCS_SOURCE_DIR/" >/dev/null 2>&1
   local imagefiles=`ls $DOCS_IMAGE_DIR/*`
   popd >/dev/null 2>&1
@@ -48,7 +69,7 @@ function docs_add_changed_images {
   echo
 }
 
-function docs_add_changed_html_files {
+function docs_collect_changed_html_files {
   pushd "$DOCS_SOURCE_DIR/" >/dev/null 2>&1
   local htmlfiles=`ls *.html`
   popd >/dev/null 2>&1
@@ -63,6 +84,14 @@ function docs_add_changed_html_files {
   echo
 }
 
+function docs_add_changed_files {
+  for file in $DOCS_FILE_LIST ; do
+    /bin/cp "$DOCS_SOURCE_DIR/$file" "$DOCS_DEST_DIR/$file"
+    echo "git add -f \"$DOCS_DEST_DIR/$file\""
+    git add -f "$DOCS_DEST_DIR/$file"
+  done
+}
+
 #######################
 cd "`dirname $0`/.."
 
@@ -75,28 +104,21 @@ fi
 
 echo "# SecHub website: Build and publish"
 "$WEBSITE_HOME"/build-sechub-website.sh
+echo
+
+# Get list of changes files
 website_add_changed_files
 
-# Copy files to destination and stage them for commit
-for file in $WEBSITE_FILE_LIST ; do
-  /bin/cp "$WEBSITE_BUILD_DIR/$file" "$DEST_DIR/$file"
-  echo "git add \"$DEST_DIR/$file\""
-  git add "$DEST_DIR/$file"
-done
+# Update _nuxt directory
+website_add_nuxt_files
 
-exit 0  # Remove after development
+# Docs: Update images directory (changed files only)
+docs_collect_changed_images
 
-echo "# Docs: Update images directory (changed files only)"
-docs_add_changed_images
+# Docs: Add changed html files
+docs_collect_changed_html_files
 
-echo "# Docs: Add changed html files"
-docs_add_changed_html_files
-
-# Copy files to destination and stage them for commit
-for file in $DOCS_FILE_LIST ; do
-  /bin/cp "$DOCS_SOURCE_DIR/$file" "$DOCS_DEST_DIR/$file"
-  echo "git add -f \"$DOCS_DEST_DIR/$file\""
-  git add -f "$DOCS_DEST_DIR/$file"
-done
+# Copy docs files to destination and stage them for commit
+docs_add_changed_files
 
 # Important: We do no git commit here - so everything the script does, can be undone.
