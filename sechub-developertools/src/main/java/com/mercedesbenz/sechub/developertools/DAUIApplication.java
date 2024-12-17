b8a2f784bc571bcca9cdcf38ca88b296be2304a8
Albert Tregnaghi <albert.tregnaghi@mercedes-benz.com>
2024-12-17 17:10:41 +0100

    Fix DAUI fat jar problem #3754
    - introduced DAUI as Spring Boot Application
    - removed old custom gradle tasks to build the DAUI
    - updated documentation
    - removed every usage of buildDeveloperAdminUI

---
+++ sechub-developertools/src/main/java/com/mercedesbenz/sechub/developertools/DAUIApplication.java
@@ -0,0 +1,16 @@
+package com.mercedesbenz.sechub.developertools;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class) // we do not want to have automatic resources in HAL & co
+public class DAUIApplication {
+
+    public static void main(String[] args) {
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(DAUIApplication.class);
+        builder.headless(false);
+
+        builder.run(args);
+    }
+}
