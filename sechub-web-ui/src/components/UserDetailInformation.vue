a610e6f0ca0a741e68e356bfa6115b96ede99481
hamidonos <hamid.ahmetovic@mercedes-benz.com>
2025-01-17 13:01:05 +0100

    implement profile page in web ui #3584

---
+++ sechub-web-ui/src/components/UserDetailInformation.vue
@@ -0,0 +1,28 @@
+<!-- SPDX-License-Identifier: MIT -->
+<template>
+  <v-card>
+    Hallo
+  </v-card>
+</template>
+
+<script lang="ts">
+  import defaultClient from '@/services/defaultClient'
+  import { useRouter } from 'vue-router'
+  import { ref } from 'vue'
+
+  export default {
+    name: 'UserDetailInformation',
+
+    setup () {
+    },
+  }
+</script>
+
+<style scoped>
+  .project-owned {
+    color: rgb(var(--v-theme-primary)) !important;
+  }
+  .v-list-item{
+    background-color: rgb(var(--v-theme-layer_01)) !important;
+  }
+</style>
