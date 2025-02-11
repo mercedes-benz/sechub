3722f90749d94e077e59be5d16564069590f0830
Jan Winz <jan.winz@mercedes-benz.com>
2025-02-11 10:32:30 +0100

    update build steps #3854

--- ide-plugins/jetbrains/build-dependencies.sh
+++ ide-plugins/jetbrains/build-dependencies.sh
@@ -8,7 +8,8 @@ JAVA_API_JAR_TO_CREATE="ide-plugins/jetbrains/lib/sechub-java-api-all-0.0.0.jar"
 cd "../../"
 
 if [ ! -f "$JAVA_API_JAR_TO_CREATE" ] || [ "$ARG" = "rebuildJar" ]; then
-    ./gradlew buildJavaApiAll
+    ./gradlew :sechub-api-java:build
+    ./gradlew :sechub-api-java:buildJavaApiAll
     cp "$JAVA_API_JAR_TO_COPY" "$JAVA_API_JAR_TO_CREATE"
 fi
 
