34402157fe32a3f00d36449a332000ec672526e3
winzj <86345915+winzj@users.noreply.github.com>
2025-02-18 07:20:15 +0100

    Move sechub intellij plugin to sechub repository #3854 (#3857)
    Move sechub intellij plugin to sechub repository
    - Update build.gradle
    - Move workflow file for plugin build
    - Update versions of actions in build and add excludes
    - Add gradle wrapper
    - update build steps
    - move gitignore for gradle wrapper

---
+++ ide-plugins/jetbrains/src/main/java-intellij/com/mercedesbenz/sechub/plugin/idea/IntellijRenderDataProvider.java
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+package com.mercedesbenz.sechub.plugin.idea;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.util.IconLoader;
+import com.mercedesbenz.sechub.commons.model.ScanType;
+import com.mercedesbenz.sechub.commons.model.Severity;
+import com.mercedesbenz.sechub.commons.model.TrafficLight;
+import com.mercedesbenz.sechub.plugin.ui.FindingRenderDataProvider;
+
+import javax.swing.*;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class IntellijRenderDataProvider implements FindingRenderDataProvider {
+
+    private Map<String, Icon> iconMap = new TreeMap<>();
+
+    @Override
+    public Icon getIconForScanType(ScanType scanType) {
+        if (scanType == null) {
+            return AllIcons.Nodes.Unknown;
+        }
+        switch (scanType) {
+            case CODE_SCAN -> {
+                return AllIcons.FileTypes.Any_type;
+            }
+            case WEB_SCAN -> {
+                return AllIcons.General.Web;
+            }
+            case INFRA_SCAN -> {
+                return AllIcons.Webreferences.Server;
+            }
+            case LICENSE_SCAN -> {
+                return AllIcons.General.TodoImportant;
+            }
+            case SECRET_SCAN -> {
+                return AllIcons.CodeWithMe.CwmPermissions;
+            }
+            case REPORT -> {
+                return AllIcons.FileTypes.Properties;
+            }
+            case ANALYTICS -> {
+                return AllIcons.General.InspectionsEye;
+            }
+            case UNKNOWN -> {
+                return AllIcons.FileTypes.Unknown;
+            }
+        }
+        return AllIcons.FileTypes.Any_type;
+    }
+
+    @Override
+    public Icon getIconForTrafficLight(TrafficLight trafficLight) {
+        if (trafficLight==null){
+            return null;
+        }
+        switch (trafficLight){
+            case GREEN -> {
+                return findIcon("/icons/trafficlight-green.svg");
+            }
+            case YELLOW -> {
+                return findIcon("/icons/trafficlight-yellow.svg");
+            }
+            case RED -> {
+                return findIcon("/icons/trafficlight-red.svg");
+            }
+            case OFF -> {
+                return findIcon("/icons/trafficlight-off.svg");
+            }
+        }
+        return null;
+    }
+
+    private Icon findIcon(String path){
+        return iconMap.computeIfAbsent(path, iconPath->IconLoader.findIcon(iconPath, getClass().getClassLoader()));
+    }
+
+}
---
+++ ide-plugins/jetbrains/src/test/resources/explorer/scenario1/project1/src/test/java/com/example/subpackage/SameName.java
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+package explorer.scenario1.project1.src.test.java.com.example.subpackage;
+
+public class SameName {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
