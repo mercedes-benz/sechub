616a2f82c34ec6f6ce177b16491e19aa579249a8
Jan Winz <jan.winz@mercedes-benz.com>
2025-03-26 07:52:35 +0100

    improve test readability #3602

--- github-actions/scan/__test__/fs-helper.test.ts
+++ github-actions/scan/__test__/fs-helper.test.ts
@@ -16,13 +16,16 @@ describe('deleteDirectoryExceptGivenFile', () => {
     });
 
     it('should move the file to a temporary location, delete the directory, recreate it, and move the file back', async () => {
+        /* prepare */
         (fs_extra.existsSync as jest.Mock).mockReturnValue(true);
         (fs_extra.move as jest.Mock).mockResolvedValue(undefined);
         (fs_extra.remove as jest.Mock).mockResolvedValue(undefined);
         (fs_extra.ensureDir as jest.Mock).mockResolvedValue(undefined);
 
+        /* execute */
         await deleteDirectoryExceptGivenFile(directoryToCleanUp, fileToKeep);
 
+        /* test */
         expect(fs_extra.move).toHaveBeenCalledWith(path.resolve(fileToKeep), tempFile);
         expect(fs_extra.remove).toHaveBeenCalledWith(directoryToCleanUp);
         expect(fs_extra.ensureDir).toHaveBeenCalledWith(path.dirname(path.resolve(fileToKeep)));
@@ -30,10 +33,13 @@ describe('deleteDirectoryExceptGivenFile', () => {
     });
 
     it('should not perform any operations if the file to keep does not exist', async () => {
+        /* prepare */
         (fs_extra.existsSync as jest.Mock).mockReturnValue(false);
 
+        /* execute */
         await deleteDirectoryExceptGivenFile(directoryToCleanUp, fileToKeep);
 
+        /* test */
         expect(fs_extra.move).not.toHaveBeenCalled();
         expect(fs_extra.remove).not.toHaveBeenCalled();
         expect(fs_extra.ensureDir).not.toHaveBeenCalled();
