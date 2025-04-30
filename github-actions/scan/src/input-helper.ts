53ded4a4f7fe75e195b36f88cb89944124122d74
winzj <86345915+winzj@users.noreply.github.com>
2025-04-30 09:29:02 +0200

    Add proxy to client download using http(s)_proxy #4060 (#4123)
    * Add proxy to client download using http(s)_proxy #4060

--- github-actions/scan/src/input-helper.ts
+++ github-actions/scan/src/input-helper.ts
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MIT
 
+import { AxiosProxyConfig } from "axios";
+import { handleError } from "./action-helper";
+
 const COMMA = ',';
 
 /**
@@ -15,4 +18,66 @@ export function split(input: string): string[] {
         .split(COMMA)
         .map(item => item.trim())
         .filter(item => item.length > 0);
+}
+
+/**
+ * This method checks the default environment variables 'http_proxy' and 'https_proxy' for http/https proxy specification.
+ * The variables are expected to be specified in this format: http_proxy='http://user:password@proxy.example.org:1234'.
+ * This results in an URL object like this:
+ * URL {
+      href: 'http://user:password@proxy.example.org:1234/',
+      origin: 'http://proxy.example.org:1234',
+      protocol: 'http:',
+      username: 'user',
+      password: 'password',
+      host: 'proxy.example.org:1234',
+      hostname: 'proxy.example.org',
+      port: '1234',
+      pathname: '/',
+      search: '',
+      searchParams: URLSearchParams {},
+      hash: ''
+    }
+ * @returns configured AxiosProxyConfig or undefined if no proxy was
+ * @throws error if the proxy URL inside the ENV variable is an invalid URL
+ */
+export function resolveProxyConfig(): AxiosProxyConfig | undefined {
+    const httpsProxy = process.env.https_proxy;
+    const httpProxy = process.env.http_proxy;
+
+    const proxy = httpsProxy || httpProxy;
+    
+    if (!proxy) {
+        return undefined;
+    }
+    try {
+        const proxyUrl = new URL(proxy);
+        const proxyConfig = {
+            protocol: proxyUrl.protocol.replace(':', ''), // Remove the trailing colon
+            host: proxyUrl.hostname,
+            port: proxyUrl.port ? parseInt(proxyUrl.port, 10) : getProtocolDefaultPort(proxyUrl.protocol),
+            ...(proxyUrl.username || proxyUrl.password ? {
+                auth: {
+                    username: proxyUrl.username,
+                    password: proxyUrl.password
+                }
+            } : undefined)
+        };
+        return proxyConfig;
+    } catch(error: any) {
+        throw new Error(`Trying to setup proxy configuration received the error: "${error.message}". Make sure to use the following syntax: http://user:password@proxy.example.org:1234 or without credentials: http://proxy.example.org:1234`);
+    }
+}
+
+function getProtocolDefaultPort(protocol: string) {
+    if (!protocol){
+        throw new Error('No protocol defined!');
+    }
+    if (protocol.startsWith('https')) {
+        return 443;
+    } else if (protocol.startsWith('http')) {
+        return 80;
+    } else {
+        throw new Error('Accepted protocols are "http" and "https"');
+    }
 }
