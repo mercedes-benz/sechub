a192b5f035fc029f330d0e547093965aef793fc3
Sven Dolderer <sven.dolderer@mercedes-benz.com>
2025-05-15 12:59:54 +0200

    use local Helm chart #4165
    this also better for development

--- sechub-solutions-shared/kubernetes/10_deploy_sechub_server.sh
+++ sechub-solutions-shared/kubernetes/10_deploy_sechub_server.sh
@@ -7,9 +7,5 @@ source include.sh
 HELMCHART_NAME=$(echo $SECHUB_SERVER_HELMCHART | awk -F'/' '{print $NF}')
 
 cd "$SECHUB_K8S_BUILDDIR/10_deploy_sechub_server/"
-if [ ! -d $HELMCHART_NAME ] ; then
-  pull_and_extract_helm_chart "$SECHUB_SERVER_HELMCHART"
-fi
-
 echo "### Installing SecHub server image $SECHUB_SERVER_IMAGE_REGISTRY:$SECHUB_SERVER_IMAGE_TAG via Helm"
-helm_install_or_upgrade $HELMCHART_NAME values.yaml
+helm_install_or_upgrade $HELMCHART_NAME "../../$SECHUB_SERVER_HELMCHART" values.yaml
