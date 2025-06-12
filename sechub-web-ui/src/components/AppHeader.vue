c2ad29d5613f7324fabf274fa4a5369cf7e0c6b9
Laura <laura.bottner@mercedes-benz.com>
2025-06-12 07:51:35 +0200

    Resetting storages when logout #4225

--- sechub-web-ui/src/components/AppHeader.vue
+++ sechub-web-ui/src/components/AppHeader.vue
@@ -59,6 +59,11 @@
   import { useFetchUserDetail } from '@/composables/useUserDetail'
   import { useConfig } from '@/config'
 
+  import { useProjectStore } from '@/stores/projectStore'
+  import { useReportStore } from '@/stores/reportStore'
+  import { useUserDetailInformationStore } from '@/stores/userDetailInformationStore'
+  import { useTmpFalsePositivesStore } from '@/stores/tempFalsePositivesStore'
+
   export default {
     name: 'AppHeader',
 
@@ -66,6 +71,11 @@
       const { t } = useI18n()
       const router = useRouter()
       const config = useConfig()
+
+      const projectStore = useProjectStore()
+      const reportStore = useReportStore()
+      const userDetailInformationStore = useUserDetailInformationStore()
+      const tempFalsePositivesStore = useTmpFalsePositivesStore()
 
       const faqLink = ref(config.value.SECHUB_FAQ_LINK)
 
@@ -95,6 +105,12 @@
       }
 
       async function logout () {
+        // Reset all storages
+        projectStore.$reset()
+        reportStore.$reset()
+        tempFalsePositivesStore.$reset()
+        userDetailInformationStore.$reset()
+
         // Perform a full browser navigation to /logout so the nginx redirect is followed
         window.location.href = '/logout'
       }
