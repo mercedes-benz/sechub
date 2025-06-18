d7b2c02ffb428f1d8fc772f749d6c6822f83c568
Laura <laura.bottner@mercedes-benz.com>
2025-06-18 13:05:05 +0200

    Removed baseUrl #4248

--- sechub-website/nuxt.config.ts
+++ sechub-website/nuxt.config.ts
@@ -1,11 +1,8 @@
 // SPDX-License-Identifier: MIT
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-const baseURL = process.env.NODE_ENV === 'development' ? '' : '/sechub';
-
 export default defineNuxtConfig({
   app: {
-    baseURL,
     head: {
       htmlAttrs: { lang: 'en' },
       title: 'SecHub'
