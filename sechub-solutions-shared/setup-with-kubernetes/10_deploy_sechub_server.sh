4e56ec307dfe475a86183a9e8a29fd5edda34d27
Sven Dolderer <sven.dolderer@mercedes-benz.com>
2025-07-14 16:20:29 +0200

    optimized cd command #4282

--- sechub-solutions-shared/setup-with-kubernetes/10_deploy_sechub_server.sh
+++ sechub-solutions-shared/setup-with-kubernetes/10_deploy_sechub_server.sh
@@ -6,7 +6,7 @@ source include.sh
 
 HELMCHART_NAME=$(echo $SECHUB_SERVER_HELMCHART | awk -F'/' '{print $NF}')
 
-pushd "$SECHUB_K8S_BUILDDIR/10_deploy_sechub_server/" >/dev/null 2>&1
+cd "$SECHUB_K8S_BUILDDIR/10_deploy_sechub_server/"
 echo "### Installing SecHub server image $SECHUB_SERVER_IMAGE_REGISTRY:$SECHUB_SERVER_IMAGE_TAG via Helm"
 helm_install_or_upgrade $HELMCHART_NAME "../../$SECHUB_SERVER_HELMCHART" values.yaml
 
